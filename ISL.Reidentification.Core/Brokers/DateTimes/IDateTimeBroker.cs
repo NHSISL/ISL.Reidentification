4775e76d378a5468f22e636010ddae08a17f87a1
Christo du Toit <christo@dutoit.co.uk>
2024-09-03 11:17:37 +0100

    BROKERS: Date Time Broker

---
+++ ISL.Reidentification.Core/Brokers/DateTimes/IDateTimeBroker.cs
@@ -0,0 +1,13 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+using System;
+using System.Threading.Tasks;
+
+namespace GitFyle.Core.Api.Brokers.DateTimes
+{
+    internal interface IDateTimeBroker
+    {
+        ValueTask<DateTimeOffset> GetCurrentDateTimeOffsetAsync();
+    }
+}
