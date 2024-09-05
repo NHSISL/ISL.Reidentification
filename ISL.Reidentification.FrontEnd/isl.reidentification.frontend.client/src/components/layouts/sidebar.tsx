07e5ec91f76b837d14968ab290516924d7a73b77
David Hayes <3583738+davidhayes03@users.noreply.github.com>
2024-09-05 09:26:15 +0100

    CODE RUB: Font Awesome icon change

--- ISL.Reidentification.FrontEnd/isl.reidentification.frontend.client/src/components/layouts/sidebar.tsx
+++ ISL.Reidentification.FrontEnd/isl.reidentification.frontend.client/src/components/layouts/sidebar.tsx
@@ -1,4 +1,4 @@
-import { faList, faHome } from '@fortawesome/free-solid-svg-icons';
+import { faFileLines, faHome } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Card, ListGroup } from 'react-bootstrap';
@@ -21,31 +21,31 @@ const SideBarComponent: React.FC = () => {
                 </ListGroup.Item>
 
                 <ListGroup.Item className="bg-light">
-                    <FontAwesomeIcon icon={faList} className="me-2" />
+                    <FontAwesomeIcon icon={faFileLines} className="me-2" />
                     <Link to="/page1/1">Page 1 (with url param)</Link>
                 </ListGroup.Item>
 
                 <ListGroup.Item className="bg-light">
-                    <FontAwesomeIcon icon={faList} className="me-2" />
+                    <FontAwesomeIcon icon={faFileLines} className="me-2" />
                     <Link to="/page2">Page 2 (Secured)</Link>
                 </ListGroup.Item>
 
                 <FeatureSwitch feature={FeatureDefinitions.Test}>
                     <ListGroup.Item className="bg-light">
-                        <FontAwesomeIcon icon={faList} className="me-2" />
+                        <FontAwesomeIcon icon={faFileLines} className="me-2" />
                         <Link to="/page3">Page 3 (Feature Switch)</Link>
                     </ListGroup.Item>
                 </FeatureSwitch>
 
                 <SecuredComponent allowedRoles={securityPoints.component1.view}>
                     <ListGroup.Item className="bg-light">
-                        <FontAwesomeIcon icon={faList} className="me-2" />
+                        <FontAwesomeIcon icon={faFileLines} className="me-2" />
                         <Link to="/page4">Page 4 (secureComponent)</Link>
                     </ListGroup.Item>
                 </SecuredComponent>
 
                 <ListGroup.Item className="bg-light">
-                    <FontAwesomeIcon icon={faList} className="me-2" />
+                    <FontAwesomeIcon icon={faFileLines} className="me-2" />
                     <Link to="/page5">Page 5 (no security)</Link>
                 </ListGroup.Item>
 
