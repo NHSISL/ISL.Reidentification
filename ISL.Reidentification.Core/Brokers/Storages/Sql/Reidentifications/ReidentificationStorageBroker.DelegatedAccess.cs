ceb2d8b4e9719a8b6dbd5df84d99158c6e4affb3
Joe Owens <joe.parkes@answerdigital.com>
2024-09-09 12:58:14 +0100

    DATA: DelegatedAccess

---
+++ ISL.Reidentification.Core/Brokers/Storages/Sql/Reidentifications/ReidentificationStorageBroker.DelegatedAccess.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using EFxceptions;
+using ISL.Reidentification.Core.Models.Foundations.DelegatedAccesses;
+using Microsoft.EntityFrameworkCore;
+
+namespace ISL.Reidentification.Core.Brokers.Storages.Sql.Reidentifications
+{
+    public partial class ReidentificationStorageBroker : EFxceptionsContext, IReidentificationStorageBroker
+    {
+       public DbSet<DelegatedAccess> DelegatedAccesses { get; set; }
+    }
+}
