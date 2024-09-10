4cf5dfdde0afb4de630f2856f12abe8245804c08
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-09-10 16:24:12 +0100

    ShouldThrowDependencyValidationExceptionOnAddIfDelegatedAccessAlreadyExistsAndLogItAsync -> FAIL

--- ISL.Reidentification.Core.Tests.Unit/Services/Foundations/DelegatedAccesses/DelegatedAccessesTests.Exceptions.Add.cs
+++ ISL.Reidentification.Core.Tests.Unit/Services/Foundations/DelegatedAccesses/DelegatedAccessesTests.Exceptions.Add.cs
@@ -2,6 +2,7 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using EFxceptions.Models.Exceptions;
 using FluentAssertions;
 using ISL.Reidentification.Core.Models.Foundations.DelegatedAccesses;
 using ISL.Reidentification.Core.Models.Foundations.DelegatedAccesses.Exceptions;
@@ -53,6 +54,61 @@ namespace ISL.Reidentification.Core.Tests.Unit.Services.Foundations.DelegatedAcc
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCriticalAsync(It.Is(SameExceptionAs(
                     expectedDelegatedAccessDependencyException))),
+                        Times.Once);
+
+            this.reidentificationStorageBroker.Verify(broker =>
+                broker.InsertDelegatedAccessAsync(It.IsAny<DelegatedAccess>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.reidentificationStorageBroker.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnAddIfDelegatedAccessAlreadyExistsAndLogItAsync()
+        {
+            // given
+            DelegatedAccess someDelegatedAccess = CreateRandomDelegatedAccess();
+
+            var duplicateKeyException =
+                new DuplicateKeyException(
+                    message: "Duplicate key error occurred");
+
+            var alreadyExistsDelegatedAccessException =
+                new AlreadyExistsDelegatedAccessException(
+                    message: "DelegatedAccess already exists error occurred.",
+                    innerException: duplicateKeyException,
+                    data: duplicateKeyException.Data);
+
+            var expectedDelegatedAccessDependencyValidationException =
+                new DelegatedAccessDependencyValidationException(
+                    message: "DelegatedAccess dependency validation error occurred, fix errors and try again.",
+                    innerException: alreadyExistsDelegatedAccessException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ThrowsAsync(duplicateKeyException);
+
+            // when
+            ValueTask<DelegatedAccess> addDelegatedAccessTask =
+                this.delegatedAccessService.AddDelegatedAccessAsync(someDelegatedAccess);
+
+            DelegatedAccessDependencyValidationException actualDelegatedAccessDependencyValidationException =
+                await Assert.ThrowsAsync<DelegatedAccessDependencyValidationException>(
+                    testCode: addDelegatedAccessTask.AsTask);
+
+            // then
+            actualDelegatedAccessDependencyValidationException.Should().BeEquivalentTo(
+                expectedDelegatedAccessDependencyValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedDelegatedAccessDependencyValidationException))),
                         Times.Once);
 
             this.reidentificationStorageBroker.Verify(broker =>
---
+++ ISL.Reidentification.Core/Models/Foundations/DelegatedAccesses/Exceptions/AlreadyExistsDelegatedAccessException.cs
@@ -0,0 +1,22 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections;
+using Xeptions;
+
+namespace ISL.Reidentification.Core.Models.Foundations.DelegatedAccesses.Exceptions
+{
+    public class AlreadyExistsDelegatedAccessException : Xeption
+    {
+        public AlreadyExistsDelegatedAccessException(
+            string message,
+            Exception innerException,
+            IDictionary data)
+                : base(message,
+                  innerException,
+                  data)
+        { }
+    }
+}
