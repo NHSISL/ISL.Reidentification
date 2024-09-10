235a288ca312e298e72280466a339802e708c515
Joe Parkes <joe.owens@answerdigital.com>
2024-09-10 13:46:35 +0100

    Add DelegatedAccessService

---
+++ ISL.Reidentification.Core/Services/Foundations/DelegatedAccesses/DelegatedAccessService.cs
@@ -0,0 +1,34 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using ISL.Reidentification.Core.Brokers.DateTimes;
+using ISL.Reidentification.Core.Brokers.Loggings;
+using ISL.Reidentification.Core.Brokers.Storages.Sql.Reidentifications;
+using ISL.Reidentification.Core.Models.Foundations.DelegatedAccesses;
+
+namespace ISL.Reidentification.Core.Services.Foundations.DelegatedAccesses
+{
+    public partial class DelegatedAccessService : IDelegatedAccessService
+    {
+        private readonly IReidentificationStorageBroker reidentificationStorageBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public DelegatedAccessService(
+            IReidentificationStorageBroker reidentificationStorageBroker,
+            IDateTimeBroker dateTimeBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.reidentificationStorageBroker = reidentificationStorageBroker;
+            this.dateTimeBroker = dateTimeBroker;
+            this.loggingBroker = loggingBroker;
+        }
+        public ValueTask<DelegatedAccess> AddDelegatedAccessAsync(DelegatedAccess delegatedAccess)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
