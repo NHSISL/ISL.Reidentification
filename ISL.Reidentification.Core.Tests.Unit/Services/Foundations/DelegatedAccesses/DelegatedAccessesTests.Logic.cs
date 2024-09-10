f3ec7d56c734cf9e88c4f4a8177f752a93187fe0
Joe Parkes <joe.owens@answerdigital.com>
2024-09-10 14:00:54 +0100

    ShouldAddDelegatedAccessAsync -> FAIL

---
+++ ISL.Reidentification.Core.Tests.Unit/Services/Foundations/DelegatedAccesses/DelegatedAccessesTests.Logic.cs
@@ -0,0 +1,42 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using FluentAssertions;
+using Force.DeepCloner;
+using ISL.Reidentification.Core.Models.Foundations.DelegatedAccesses;
+using Moq;
+
+namespace ISL.Reidentification.Core.Tests.Unit.Services.Foundations.DelegatedAccesses
+{
+    public partial class DelegatedAccessesTests
+    {
+        [Fact]
+        public async Task ShouldAddDelegatedAccessAsync()
+        {
+            //given
+            DelegatedAccess randomDelegatedAccess = CreateRandomDelegatedAccess();
+            DelegatedAccess inputDelegatedAccess = randomDelegatedAccess;
+            DelegatedAccess storageDelegatedAccess = inputDelegatedAccess.DeepClone();
+            DelegatedAccess expectedDelegatedAccess = inputDelegatedAccess.DeepClone();
+
+            this.reidentificationStorageBroker.Setup(broker =>
+                broker.InsertDelegatedAccessAsync(inputDelegatedAccess))
+                    .ReturnsAsync(storageDelegatedAccess);
+
+            //when
+            DelegatedAccess actualDelegatedAccess = await this.delegatedAccessService.AddDelegatedAccessAsync(inputDelegatedAccess);
+
+            //then
+            actualDelegatedAccess.Should().BeEquivalentTo(expectedDelegatedAccess);
+
+            this.reidentificationStorageBroker.Verify(broker =>
+                broker.InsertDelegatedAccessAsync(inputDelegatedAccess),
+                    Times.Once);
+
+            this.reidentificationStorageBroker.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ ISL.Reidentification.Core.Tests.Unit/Services/Foundations/DelegatedAccesses/DelegatedAccessesTests.cs
@@ -0,0 +1,57 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using ISL.Reidentification.Core.Brokers.DateTimes;
+using ISL.Reidentification.Core.Brokers.Loggings;
+using ISL.Reidentification.Core.Brokers.Storages.Sql.Reidentifications;
+using ISL.Reidentification.Core.Models.Foundations.DelegatedAccesses;
+using ISL.Reidentification.Core.Services.Foundations.DelegatedAccesses;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace ISL.Reidentification.Core.Tests.Unit.Services.Foundations.DelegatedAccesses
+{
+    public partial class DelegatedAccessesTests
+    {
+        private readonly Mock<IReidentificationStorageBroker> reidentificationStorageBroker;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly DelegatedAccessService delegatedAccessService;
+
+        public DelegatedAccessesTests()
+        {
+            this.reidentificationStorageBroker = new Mock<IReidentificationStorageBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.delegatedAccessService = new DelegatedAccessService(
+                reidentificationStorageBroker.Object,
+                dateTimeBrokerMock.Object,
+                loggingBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static DelegatedAccess CreateRandomDelegatedAccess() =>
+            CreateRandomDelegatedAccesses(dateTimeOffset: GetRandomDateTimeOffset());
+
+        private static DelegatedAccess CreateRandomDelegatedAccesses(DateTimeOffset dateTimeOffset) =>
+            CreateDelegatedAccessesFiller(dateTimeOffset).Create();
+
+        private static Filler<DelegatedAccess> CreateDelegatedAccessesFiller(DateTimeOffset dateTimeOffset)
+        {
+            string user = Guid.NewGuid().ToString();
+            var filler = new Filler<DelegatedAccess>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset)
+                .OnProperty(delegatedAccess => delegatedAccess.CreatedBy).Use(user)
+                .OnProperty(delegatedAccess => delegatedAccess.UpdatedBy).Use(user);
+
+            return filler;
+        }
+    }
+}
