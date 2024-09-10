a47909d0fe139102d2d0d8f7947bfc1d97f0dc18
Joe Parkes <joe.owens@answerdigital.com>
2024-09-10 15:00:51 +0100

    CODE RUB: Make Data field of DelegatedAccess nullable

--- ISL.Reidentification.Core/Models/Foundations/DelegatedAccesses/DelegatedAccess.cs
+++ ISL.Reidentification.Core/Models/Foundations/DelegatedAccesses/DelegatedAccess.cs
@@ -1,3 +1,7 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
 using System;
 
 namespace ISL.Reidentification.Core.Models.Foundations.DelegatedAccesses
@@ -10,7 +14,7 @@ namespace ISL.Reidentification.Core.Models.Foundations.DelegatedAccesses
         public string RecipientEmail { get; set; }
         public bool IsDelegatedAccess { get; set; }
         public bool IsApproved { get; set; }
-        public byte[] Data { get; set; }
+        public byte[]? Data { get; set; }
         public string IdentifierColumn { get; set; }
         public string CreatedBy { get; set; }
         public DateTimeOffset CreatedDate { get; set; }
