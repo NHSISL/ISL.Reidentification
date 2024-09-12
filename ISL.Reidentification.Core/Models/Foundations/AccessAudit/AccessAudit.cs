fa9d57f4c0ac44ff397ea5813a86a60ae14a2407
Hassan Raza <h_raza110@outlook.com>
2024-09-12 11:54:42 +0100

    MODEL: AccessAudit

---
+++ ISL.Reidentification.Core/Models/Foundations/AccessAudit/AccessAudit.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+
+namespace ISL.Reidentification.Core.Models.Foundations.AccessAudit
+{
+    public class AccessAudit
+    {
+        public Guid Id { get; set; }
+        public string PseudoIdentifier { get; set; }
+        public string UserEmail { get; set; }
+        public bool HasAccess { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTimeOffset CreatedWhen { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTimeOffset UpdatedWhen { get; set; }
+    }
+}
