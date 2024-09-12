c3b2e2162b69b7c07eac13176e0e03bde7cfc0f8
Joe Parkes <joe.owens@answerdigital.com>
2024-09-12 10:35:08 +0100

    ShouldThrowValidationExceptionOnModifyIfDelegatedAccessIsNullAndLogItAsync -> FAIL

---
+++ ISL.Reidentification.Core.Tests.Unit/Services/Foundations/DelegatedAccesses/DelegatedAccessesTests.Validations.Modify.cs
@@ -0,0 +1,54 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using FluentAssertions;
+using ISL.Reidentification.Core.Models.Foundations.DelegatedAccesses;
+using ISL.Reidentification.Core.Models.Foundations.DelegatedAccesses.Exceptions;
+using Moq;
+
+namespace ISL.Reidentification.Core.Tests.Unit.Services.Foundations.DelegatedAccesses
+{
+    public partial class DelegatedAccessesTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfDelegatedAccessIsNullAndLogItAsync()
+        {
+            //given
+            DelegatedAccess nullDelegatedAccess = null;
+
+            var nullDelegatedAccessException =
+                new NullDelegatedAccessException(message: "DelegatedAccess is null");
+
+            var expectedDelegatedAccessValidationException =
+                new DelegatedAccessValidationException(
+                    message: "DelegatedAccess validation error occurred, please fix errors and try again.",
+                    innerException: nullDelegatedAccessException);
+
+            // when
+            ValueTask<DelegatedAccess> modifyDelegatedAccessTask =
+                this.delegatedAccessService.ModifyDelegatedAccessAsync(nullDelegatedAccess);
+
+            DelegatedAccessValidationException actualDelegatedAccessValidationException =
+                await Assert.ThrowsAsync<DelegatedAccessValidationException>(
+                    modifyDelegatedAccessTask.AsTask);
+
+            // then
+            actualDelegatedAccessValidationException.Should().BeEquivalentTo(
+                expectedDelegatedAccessValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(
+                    SameExceptionAs(expectedDelegatedAccessValidationException))),
+                        Times.Once);
+
+            this.reidentificationStorageBroker.Verify(broker =>
+                broker.UpdateDelegatedAccessAsync(It.IsAny<DelegatedAccess>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.reidentificationStorageBroker.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
