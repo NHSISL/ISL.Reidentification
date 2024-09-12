914dbd7045de0fa10ae085b8b83f6e60f59c9a28
Joe Parkes <joe.owens@answerdigital.com>
2024-09-12 09:25:55 +0100

    ShouldModifyDelegatedAccessAsync -> FAIL

---
+++ ISL.Reidentification.Core.Tests.Unit/Services/Foundations/DelegatedAccesses/DelegatedAccessesTests.Logic.Modify.cs
@@ -0,0 +1,47 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using FluentAssertions;
+using Force.DeepCloner;
+using ISL.Reidentification.Core.Models.Foundations.DelegatedAccesses;
+using Moq;
+
+namespace ISL.Reidentification.Core.Tests.Unit.Services.Foundations.DelegatedAccesses
+{
+    public partial class DelegatedAccessesTests
+    {
+        [Fact]
+        public async Task ShouldModifyDelegatedAccessAsync()
+        {
+            //given
+            DelegatedAccess randomDelegatedAccess = CreateRandomDelegatedAccess();
+            DelegatedAccess inputDelegatedAccess = randomDelegatedAccess;
+            DelegatedAccess updatedDelegatedAccess = inputDelegatedAccess.DeepClone();
+            DelegatedAccess expectedDelegatedAccess = inputDelegatedAccess.DeepClone();
+
+            this.reidentificationStorageBroker.Setup(broker =>
+                broker.UpdateDelegatedAccessAsync(inputDelegatedAccess))
+                    .ReturnsAsync(updatedDelegatedAccess);
+
+            //when
+            DelegatedAccess actualDelegatedAccess =
+                await this.delegatedAccessService.ModifyDelegatedAccessAsync(inputDelegatedAccess);
+
+            //then
+            actualDelegatedAccess.Should().BeEquivalentTo(expectedDelegatedAccess);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.reidentificationStorageBroker.Verify(broker =>
+                broker.InsertDelegatedAccessAsync(inputDelegatedAccess),
+                    Times.Once);
+
+            this.reidentificationStorageBroker.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
