0fbfe328955685748ddd264962c39d84964abad5
Hassan Raza <h_raza110@outlook.com>
2024-09-13 12:04:24 +0100

    DATA: AccessAudit

---
+++ ISL.Reidentification.Core/Migrations/20240913110210_AddAccessAudit.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ISL.ReIdentification.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccessAudit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
