75c62c2ade8c49b73d84a1be762cf125a9ffec3c
Hassan Raza <h_raza110@outlook.com>
2024-09-13 13:18:15 +0100

    DATA: Access Audit

---
+++ ISL.Reidentification.Core/Brokers/Storages/Sql/ReIdentifications/ReidentificationStorageBroker.AccessAudit.Configurations.cs
@@ -0,0 +1,40 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using ISL.ReIdentification.Core.Models.Foundations.AccessAudits;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ISL.ReIdentification.Core.Brokers.Storages.Sql.ReIdentifications
+{
+    public partial class ReIdentificationStorageBroker
+    {
+        private void AddAccessAuditConfigurations(EntityTypeBuilder<AccessAudit> builder)
+        {
+            builder.Property(accessAudit => accessAudit.PseudoIdentifier)
+                .HasMaxLength(10)
+                .IsRequired();
+
+            builder.Property(accessAudit => accessAudit.UserEmail)
+                .HasMaxLength(320)
+                .IsRequired();
+
+            builder.Property(accessAudit => accessAudit.HasAccess)
+                .IsRequired();
+
+            builder.Property(accessAudit => accessAudit.CreatedBy)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.Property(accessAudit => accessAudit.CreatedDate)
+                .IsRequired();
+
+            builder.Property(accessAudit => accessAudit.UpdatedBy)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.Property(accessAudit => accessAudit.UpdatedDate)
+                .IsRequired();
+        }
+    }
+}
