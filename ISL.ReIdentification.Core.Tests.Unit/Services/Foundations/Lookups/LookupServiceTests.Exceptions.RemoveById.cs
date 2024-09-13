5328672cc8d1ea3b3d1c9a89ce85330ad64fc82a
Christo du Toit <christo@dutoit.co.uk>
2024-09-13 10:45:24 +0100

    ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync -> PASS

--- ISL.ReIdentification.Core.Tests.Unit/Services/Foundations/Lookups/LookupServiceTests.Exceptions.RemoveById.cs
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Foundations/Lookups/LookupServiceTests.Exceptions.RemoveById.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using ISL.ReIdentification.Core.Models.Foundations.Lookups;
 using ISL.ReIdentification.Core.Models.Foundations.Lookups.Exceptions;
@@ -58,6 +60,59 @@ namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.Lookups
 
             this.dateTimeBrokerMock.Verify(broker =>
                 broker.GetCurrentDateTimeOffset(),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someLookupId = Guid.NewGuid();
+
+            var databaseUpdateConcurrencyException =
+                new DbUpdateConcurrencyException();
+
+            var lockedLookupException =
+                new LockedLookupException(
+                    message: "Locked lookup record exception, please try again later",
+                    innerException: databaseUpdateConcurrencyException);
+
+            var expectedLookupDependencyValidationException =
+                new LookupDependencyValidationException(
+                    message: "Lookup dependency validation occurred, please try again.",
+                    innerException: lockedLookupException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectLookupByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(databaseUpdateConcurrencyException);
+
+            // when
+            ValueTask<Lookup> removeLookupByIdTask =
+                this.lookupService.RemoveLookupByIdAsync(someLookupId);
+
+            LookupDependencyValidationException actualLookupDependencyValidationException =
+                await Assert.ThrowsAsync<LookupDependencyValidationException>(
+                    removeLookupByIdTask.AsTask);
+
+            // then
+            actualLookupDependencyValidationException.Should()
+                .BeEquivalentTo(expectedLookupDependencyValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectLookupByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedLookupDependencyValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteLookupAsync(It.IsAny<Lookup>()),
                     Times.Never);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
