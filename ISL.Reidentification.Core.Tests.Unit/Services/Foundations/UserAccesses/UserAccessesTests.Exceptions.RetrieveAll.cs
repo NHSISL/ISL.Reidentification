d6e1256e56208631c558f766391671e9b6dd5c13
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-09-13 14:16:06 +0100

    ShouldThrowServiceExceptionOnRetrieveAllWhenServiceErrorOccursAndLogItAsync -> FAIL

--- ISL.Reidentification.Core.Tests.Unit/Services/Foundations/UserAccesses/UserAccessesTests.Exceptions.RetrieveAll.cs
+++ ISL.Reidentification.Core.Tests.Unit/Services/Foundations/UserAccesses/UserAccessesTests.Exceptions.RetrieveAll.cs
@@ -61,5 +61,52 @@ namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.UserAccesses
             this.ReIdentificationStorageBroker.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRetrieveAllWhenServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            Exception serviceError = new Exception();
+
+            var failedServiceUserAccessException = new FailedServiceUserAccessException(
+                message: "Failed service user access error occurred, contact support.",
+                innerException: serviceError);
+
+            var expectedUserAccessServiceException = new UserAccessServiceException(
+                message: "Service error occurred, contact support.",
+                innerException: failedServiceUserAccessException);
+
+            this.ReIdentificationStorageBroker.Setup(broker =>
+                broker.SelectAllUserAccessesAsync())
+                    .ThrowsAsync(serviceError);
+
+            // when
+            ValueTask<IQueryable<UserAccess>> retrieveAllUserAccessesTask =
+                this.userAccessService.RetrieveAllUserAccessesAsync();
+
+            UserAccessServiceException actualUserAccessServiceExcpetion =
+                await Assert.ThrowsAsync<UserAccessServiceException>(
+                    retrieveAllUserAccessesTask.AsTask);
+
+            // then
+            actualUserAccessServiceExcpetion.Should().BeEquivalentTo(expectedUserAccessServiceException);
+
+            this.ReIdentificationStorageBroker.Verify(broker =>
+                broker.SelectAllUserAccessesAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedUserAccessServiceException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.ReIdentificationStorageBroker.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
