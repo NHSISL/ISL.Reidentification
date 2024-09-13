cb2432ab263ac8fc1754e90cb7167db1f74d2b52
Christo du Toit <christo@dutoit.co.uk>
2024-09-13 10:00:12 +0100

    Delete ISL.Reidentification.Core directory

--- ISL.Reidentification.Core/Brokers/Storages/Sql/Reidentifications/IReidentificationStorageBroker.cs
+++
@@ -1,9 +0,0 @@
-﻿// ---------------------------------------------------------
-// Copyright (c) North East London ICB. All rights reserved.
-// ---------------------------------------------------------
-
-namespace ISL.ReIdentification.Core.Brokers.Storages.Sql.ReIdentifications
-{
-    public partial interface IReIdentificationStorageBroker
-    { }
-}
