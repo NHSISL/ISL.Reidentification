6e455e33a57c0bd074f9bf18284064ecca83689f
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-09-16 16:27:30 +0100

    PostUserAccessAsyncShouldReturnCreatedWhenUserAccessIsAdded -> FAIL

---
+++ ISL.Reidentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Unit/Controllers/UserAccesses/UserAccessesControllerTests.Post.cs
@@ -0,0 +1,38 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using ISL.ReIdentification.Core.Models.Foundations.UserAccesses;
+using Moq;
+using RESTFulSense.Models;
+
+namespace ISL.ReIdentification.Configurations.Server.Tests.Unit.Controllers.UserAccesses
+{
+    public partial class UserAccessesControllerTests
+    {
+        [Fact]
+        public async Task PostUserAccessAsyncShouldReturnCreatedWhenUserAccessIsAdded()
+        {
+            // given
+            UserAccess randomUserAccess = CreateRandomUserAccess();
+            UserAccess inputUserAccess = randomUserAccess;
+            UserAccess storageUserAccess = inputUserAccess.DeepClone();
+            UserAccess expectedUserAccess = storageUserAccess.DeepClone();
+
+            mockUserAccessService.Setup(service =>
+                service.AddUserAccessAsync(inputUserAccess))
+                    .ReturnsAsync(storageUserAccess);
+
+            // when
+            var result = await userAccessesController.PostUserAccessAsync(randomUserAccess);
+
+            // then
+            var createdResult = Assert.IsType<CreatedObjectResult>(result.Result);
+            createdResult.StatusCode.Should().Be(201);
+            createdResult.Value.Should().BeEquivalentTo(expectedUserAccess);
+        }
+    }
+}
---
+++ ISL.Reidentification.WebApps.Configurations/ISL.Reidentification.Configurations.Server/Controllers/UserAccessesController.cs
@@ -0,0 +1,29 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using ISL.ReIdentification.Core.Models.Foundations.UserAccesses;
+using ISL.ReIdentification.Core.Services.Foundations.UserAccesses;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace ISL.ReIdentification.Configurations.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserAccessesController : RESTFulController
+    {
+        private readonly IUserAccessService userAccessService;
+
+        public UserAccessesController(IUserAccessService userAccessService) =>
+            this.userAccessService = userAccessService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<UserAccess>> PostUserAccessAsync(UserAccess userAccess)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
