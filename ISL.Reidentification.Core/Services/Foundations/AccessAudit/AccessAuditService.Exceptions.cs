d2cc4c088c2febcdfeacb5f0a223be5360546463
Hassan Raza <h_raza110@outlook.com>
2024-09-16 13:45:19 +0100

    ShouldThrowServiceExceptionOnAddIfServiceErrorOccurredAndLogItAsync -> PASS

--- ISL.Reidentification.Core/Services/Foundations/AccessAudit/AccessAuditService.Exceptions.cs
+++ ISL.Reidentification.Core/Services/Foundations/AccessAudit/AccessAuditService.Exceptions.cs
@@ -2,9 +2,14 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
+using ISL.Reidentification.Core.Models.Foundations.AccessAudits.Exceptions;
 using ISL.ReIdentification.Core.Models.Foundations.AccessAudits;
 using ISL.ReIdentification.Core.Models.Foundations.AccessAudits.Exceptions;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Xeptions;
 
 namespace ISL.ReIdentification.Core.Services.Foundations.AccessAudits
@@ -26,6 +31,55 @@ namespace ISL.ReIdentification.Core.Services.Foundations.AccessAudits
             {
                 throw await CreateAndLogValidationExceptionAsync(invalidAccessAuditException);
             }
+            catch (NotFoundAccessAuditException notFoundAccessAuditException)
+            {
+                throw await CreateAndLogValidationExceptionAsync(notFoundAccessAuditException);
+            }
+            catch (SqlException sqlException)
+            {
+                var failedStorageAccessAuditException = new FailedStorageAccessAuditException(
+                    message: "Failed access audit storage error occurred, contact support.",
+                    innerException: sqlException);
+
+                throw await CreateAndLogCriticalDependencyExceptionAsync(failedStorageAccessAuditException);
+            }
+            catch (DuplicateKeyException duplicateKeyException)
+            {
+                var alreadyExistsAccessAuditException =
+                    new AlreadyExistsAccessAuditException(
+                        message: "Access audit already exists error occurred.",
+                        innerException: duplicateKeyException,
+                        data: duplicateKeyException.Data);
+
+                throw await CreateAndLogDependencyValidationExceptionAsync(alreadyExistsAccessAuditException);
+            }
+            catch (DbUpdateConcurrencyException dbUpdateConcurrencyException)
+            {
+                var lockedAccessAuditException =
+                    new LockedAccessAuditException(
+                        message: "Locked access audit record error occurred, please try again.",
+                        innerException: dbUpdateConcurrencyException);
+
+                throw await CreateAndLogDependencyValidationExceptionAsync(lockedAccessAuditException);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                var failedOperationAccessAuditException =
+                    new FailedOperationAccessAuditException(
+                        message: "Failed operation access audit error occurred, contact support.",
+                        innerException: dbUpdateException);
+
+                throw await CreateAndLogDependencyExceptionAsync(failedOperationAccessAuditException);
+            }
+            catch (Exception exception)
+            {
+                var failedServiceAccessAuditException =
+                    new FailedServiceAccessAuditException(
+                        message: "Failed service access audit error occurred, contact support.",
+                        innerException: exception);
+
+                throw await CreateAndLogServiceExceptionAsync(failedServiceAccessAuditException);
+            }
         }
 
         private async ValueTask<AccessAuditValidationException> CreateAndLogValidationExceptionAsync(
@@ -38,6 +92,54 @@ namespace ISL.ReIdentification.Core.Services.Foundations.AccessAudits
             await this.loggingBroker.LogErrorAsync(accessAuditValidationException);
 
             return accessAuditValidationException;
+        }
+
+        private async ValueTask<AccessAuditDependencyException> CreateAndLogCriticalDependencyExceptionAsync(
+           Xeption exception)
+        {
+            var userAccessDependencyException = new AccessAuditDependencyException(
+                message: "Access audit dependency error occurred, contact support.",
+                innerException: exception);
+
+            await this.loggingBroker.LogCriticalAsync(userAccessDependencyException);
+
+            return userAccessDependencyException;
+        }
+
+        private async ValueTask<AccessAuditDependencyValidationException>
+            CreateAndLogDependencyValidationExceptionAsync(Xeption exception)
+        {
+            var userAccessDependencyValidationException = new AccessAuditDependencyValidationException(
+                message: "Access audit dependency validation error occurred, fix errors and try again.",
+                innerException: exception);
+
+            await this.loggingBroker.LogErrorAsync(userAccessDependencyValidationException);
+
+            return userAccessDependencyValidationException;
+        }
+
+        private async ValueTask<AccessAuditDependencyException> CreateAndLogDependencyExceptionAsync(
+            Xeption exception)
+        {
+            var userAccessDependencyException = new AccessAuditDependencyException(
+                message: "Access audit dependency error occurred, contact support.",
+                innerException: exception);
+
+            await this.loggingBroker.LogErrorAsync(userAccessDependencyException);
+
+            return userAccessDependencyException;
+        }
+
+        private async ValueTask<AccessAuditServiceException> CreateAndLogServiceExceptionAsync(
+           Xeption exception)
+        {
+            var userAccessServiceException = new AccessAuditServiceException(
+                message: "Service error occurred, contact support.",
+                innerException: exception);
+
+            await this.loggingBroker.LogErrorAsync(userAccessServiceException);
+
+            return userAccessServiceException;
         }
     }
 }
