36b383523c482b4a6e71664e9fa1f168f3dff037
Joe Parkes <joe.owens@answerdigital.com>
2024-09-16 13:53:16 +0100

    ShouldThrowServiceExceptionOnRetrieveByIdIfServiceErrorOccursAndLogItAsync -> PASS

--- ISL.ReIdentification.Core.Tests.Unit/Services/Foundations/DelegatedAccesses/DelegatedAccessesTests.Exceptions.RetrieveById.cs
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Foundations/DelegatedAccesses/DelegatedAccessesTests.Exceptions.RetrieveById.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Threading.Tasks;
+using FluentAssertions;
 using ISL.ReIdentification.Core.Models.Foundations.DelegatedAccesses;
 using ISL.ReIdentification.Core.Models.Foundations.DelegatedAccesses.Exceptions;
 using Moq;
@@ -48,6 +49,57 @@ namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.DelegatedAcc
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCriticalAsync(It.Is(SameExceptionAs(
                     expectedDelegatedAccessDependencyException))),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.reIdentificationStorageBroker.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRetrieveByIdIfServiceErrorOccursAndLogItAsync()
+        {
+            //given
+            var someDelegatedAccessId = Guid.NewGuid();
+            var serviceException = new Exception();
+
+            var failedServiceDelegatedAccessException =
+                new FailedServiceDelegatedAccessException(
+                    message: "Failed service delegated access error occurred, contact support.",
+                    innerException: serviceException);
+
+            var expectedDelegatedAccessServiceException =
+                new DelegatedAccessServiceException(
+                    message: "Service error occurred, contact support.",
+                    innerException: failedServiceDelegatedAccessException);
+
+            this.reIdentificationStorageBroker.Setup(broker =>
+                broker.SelectDelegatedAccessByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(serviceException);
+
+            //when
+            ValueTask<DelegatedAccess> retrieveDelegatedAccessByIdTask =
+                this.delegatedAccessService.RetrieveDelegatedAccessByIdAsync(someDelegatedAccessId);
+
+            DelegatedAccessServiceException actualDelegatedAccessServiceException =
+                await Assert.ThrowsAsync<DelegatedAccessServiceException>(
+                    retrieveDelegatedAccessByIdTask.AsTask);
+
+            //then
+            actualDelegatedAccessServiceException.Should().BeEquivalentTo(
+                expectedDelegatedAccessServiceException);
+
+            this.reIdentificationStorageBroker.Verify(broker =>
+                broker.SelectDelegatedAccessByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+            broker.LogErrorAsync(It.Is(SameExceptionAs(
+                expectedDelegatedAccessServiceException))),
                     Times.Once);
 
             this.dateTimeBrokerMock.Verify(broker =>
