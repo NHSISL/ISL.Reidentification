cce44baa2ff7e58a301226d15095fd770c0ba8c0
Hassan Raza <h_raza110@outlook.com>
2024-09-16 09:58:42 +0100

    FOUNDATIONS: Access Audit Add

---
+++ ISL.Reidentification.Core/Services/Foundations/AccessAudit/AccessAuditService.cs
@@ -0,0 +1,32 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using ISL.ReIdentification.Core.Brokers.DateTimes;
+using ISL.ReIdentification.Core.Brokers.Loggings;
+using ISL.ReIdentification.Core.Brokers.Storages.Sql.ReIdentifications;
+using ISL.ReIdentification.Core.Models.Foundations.AccessAudits;
+
+namespace ISL.ReIdentification.Core.Services.Foundations.AccessAudits
+{
+    public partial class AccessAuditService : IAccessAuditService
+    {
+        private readonly IReIdentificationStorageBroker reIdentificationStorageBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public AccessAuditService(
+            IReIdentificationStorageBroker reIdentificationStorageBroker,
+            IDateTimeBroker dateTimeBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.reIdentificationStorageBroker = reIdentificationStorageBroker;
+            this.dateTimeBroker = dateTimeBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<AccessAudit> AddAccessAuditAsync(AccessAudit accessAudit) =>
+            throw new System.NotImplementedException();
+    }
+}
---
+++ ISL.Reidentification.Core/Services/Foundations/AccessAudit/IAccessAuditService.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using ISL.ReIdentification.Core.Models.Foundations.AccessAudits;
+
+namespace ISL.ReIdentification.Core.Services.Foundations.AccessAudits
+{
+    public interface IAccessAuditService
+    {
+        ValueTask<AccessAudit> AddAccessAuditAsync(AccessAudit accessAudit);
+    }
+}
