7d5d6d3105d9430a281e5ae2a2d27be0ed5f3666
Christo du Toit <christo@dutoit.co.uk>
2024-09-16 14:47:27 +0100

    DATA: Pds model

---
+++ ISL.ReIdentification.Core/Models/Foundations/PdsDatas/PdsData.cs
@@ -0,0 +1,13 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+
+namespace ISL.ReIdentification.Core.Models.Foundations.PdsDatas
+{
+    public class PdsData : IKey
+    {
+        public Guid Id { get; set; }
+    }
+}
