8d1abaa4d132e478757143b07ae16bfc4a51ce60
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-09-16 11:53:16 +0100

    ShouldThrowValidationExceptionOnRemoveByIdUserAccessAsync -> FAIL

---
+++ ISL.Reidentification.Core.Tests.Unit/Services/Foundations/UserAccesses/UserAccessesTests.Validations.RemoveById.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ISL.ReIdentification.Core.Models.Foundations.UserAccesses;
+using ISL.ReIdentification.Core.Models.Foundations.UserAccesses.Exceptions;
+using Moq;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.UserAccesses
+{
+    public partial class UserAccessesTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRemoveByIdUserAccessAsync()
+        {
+            // given
+            Guid invalidUserAccessId = Guid.Empty;
+
+            var invalidUserAccessException = new InvalidUserAccessException(
+                message: "Invalid user access. Please correct the errors and try again.");
+
+            invalidUserAccessException.AddData(
+                key: nameof(UserAccess.Id),
+                values: "Id is invalid");
+
+            var expectedUserAccessValidationException =
+                new UserAccessValidationException(
+                    message: "UserAccess validation error occurred, please fix errors and try again.",
+                    innerException: invalidUserAccessException);
+
+            // when
+            ValueTask<UserAccess> removeByIdUserAccessTask = this.userAccessService
+                .RemoveUserAccessByIdAsync(invalidUserAccessId);
+
+            UserAccessValidationException actualUserAccessValidationException =
+                await Assert.ThrowsAsync<UserAccessValidationException>(removeByIdUserAccessTask.AsTask);
+
+            // then
+            actualUserAccessValidationException.Should().BeEquivalentTo(expectedUserAccessValidationException);
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(expectedUserAccessValidationException))),
+                    Times.Once());
+
+            this.reIdentificationStorageBroker.Verify(broker =>
+                broker.SelectUserAccessByIdAsync(invalidUserAccessId),
+                    Times.Never);
+
+            this.reIdentificationStorageBroker.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
