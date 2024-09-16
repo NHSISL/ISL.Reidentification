1ec16fedab9b2dc3b85564ff8f9c82285d4f3252
Hassan Raza <h_raza110@outlook.com>
2024-09-16 13:45:03 +0100

    ShouldThrowServiceExceptionOnAddIfServiceErrorOccurredAndLogItAsync -> FAIL

---
+++ ISL.Reidentification.Core/Models/Foundations/AccessAudit/Exceptions/FailedStorageAccessAuditException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace ISL.ReIdentification.Core.Models.Foundations.AccessAudits.Exceptions
+{
+    public class FailedStorageAccessAuditException : Xeption
+    {
+        public FailedStorageAccessAuditException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
