6579744a21bf3df8aa497a37bbd7ccebf5fb606e
Hassan Raza <h_raza110@outlook.com>
2024-09-16 10:10:47 +0100

    ShouldAddAccessAuditAsync -> FAIL

---
+++ ISL.Reidentification.Core.Tests.Unit/Services/Foundations/AccessAudit/UserAccessesTests.cs
@@ -0,0 +1,79 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using ISL.ReIdentification.Core.Brokers.DateTimes;
+using ISL.ReIdentification.Core.Brokers.Loggings;
+using ISL.ReIdentification.Core.Brokers.Storages.Sql.ReIdentifications;
+using ISL.ReIdentification.Core.Models.Foundations.AccessAudits;
+using ISL.ReIdentification.Core.Models.Foundations.UserAccesses;
+using ISL.ReIdentification.Core.Services.Foundations.AccessAudits;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Tynamix.ObjectFiller;
+using Xeptions;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.AccessAudits
+{
+    public partial class AccessAuditTests
+    {
+        private readonly Mock<IReIdentificationStorageBroker> reIdentificationStorageBroker;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly AccessAuditService accessAuditService;
+
+        public AccessAuditTests()
+        {
+            this.reIdentificationStorageBroker = new Mock<IReIdentificationStorageBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.accessAuditService = new AccessAuditService(
+                reIdentificationStorageBroker.Object,
+                dateTimeBrokerMock.Object,
+                loggingBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static AccessAudit CreateRandomAccessAudit() =>
+            CreateRandomAccessAudit(dateTimeOffset: GetRandomDateTimeOffset());
+
+        private static UserAccess CreateRandomAccessAudit(DateTimeOffset dateTimeOffset) =>
+            CreateAccessAuditFiller(dateTimeOffset).Create();
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+
+        private static int GetRandomNumber() =>
+            new IntRange(max: 15, min: 2).GetValue();
+
+        private SqlException CreateSqlException() =>
+            (SqlException)RuntimeHelpers.GetUninitializedObject(type: typeof(SqlException));
+
+        private static Filler<UserAccess> CreateAccessAuditFiller(DateTimeOffset dateTimeOffset)
+        {
+            string user = Guid.NewGuid().ToString();
+            var filler = new Filler<UserAccess>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset)
+                .OnProperty(userAccess => userAccess.CreatedBy).Use(user)
+                .OnProperty(userAccess => userAccess.UpdatedBy).Use(user);
+
+            return filler;
+        }
+
+        private static Expression<Func<Xeption, bool>> SameExceptionAs(
+            Xeption expectedException)
+        {
+            return actualException =>
+                actualException.SameExceptionAs(expectedException);
+        }
+    }
+}
