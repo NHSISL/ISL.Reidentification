6a566c6f8efec215a7198c665a44469199128a00
Joe Parkes <joe.owens@answerdigital.com>
2024-09-17 13:59:25 +0100

    GetLookupByIdsAsyncShouldReturnLookup -> FAIL

---
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Unit/Controllers/DelegatedAccesses/DelegatedAccessesControllerTests.Get.cs
@@ -0,0 +1,39 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using ISL.ReIdentification.Core.Models.Foundations.DelegatedAccesses;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace ISL.ReIdentification.Configurations.Server.Tests.Unit.Controllers.DelegatedAccesses
+{
+    public partial class DelegatedAccessesControllerTests
+    {
+        [Fact]
+        public async Task GetLookupByIdsAsyncShouldReturnLookup()
+        {
+            // given
+            DelegatedAccess randomDelegatedAccess = CreateRandomDelegatedAccess();
+            Guid inputId = randomDelegatedAccess.Id;
+            DelegatedAccess storageDelegatedAccess = randomDelegatedAccess.DeepClone();
+            DelegatedAccess expectedDelegatedAccess = storageDelegatedAccess.DeepClone();
+
+            this.mockDelegatedAccessService
+                .Setup(service => service.RetrieveDelegatedAccessByIdAsync(inputId))
+                .ReturnsAsync(storageDelegatedAccess);
+
+            // when
+            var result = await delegatedAccessesController.GetDelegatedAccessByIdAsync(inputId);
+
+            // then
+            var actualResult = Assert.IsType<OkObjectResult>(result.Result);
+            actualResult.StatusCode.Should().Be(200);
+            actualResult.Value.Should().BeEquivalentTo(expectedDelegatedAccess);
+        }
+    }
+}
