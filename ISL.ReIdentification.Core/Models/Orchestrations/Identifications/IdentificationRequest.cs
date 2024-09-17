0bcee6be7cc3106d9dd40f3955ad4ab08e58a106
Christo du Toit <christo@dutoit.co.uk>
2024-09-17 12:03:57 +0100

    DATA: Identification Request

---
+++ ISL.ReIdentification.Core/Models/Orchestrations/Identifications/IdentificationRequest.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+namespace ISL.ReIdentification.Core.Models.Orchestrations.Identifications
+{
+    public class IdentificationRequest
+    {
+        public int RowNumber { get; set; }
+        public string Identifier { get; set; }
+        public string UserEmail { get; set; }
+        public bool HasAccess { get; set; }
+        public bool IsReidentified { get; set; }
+    }
+}
