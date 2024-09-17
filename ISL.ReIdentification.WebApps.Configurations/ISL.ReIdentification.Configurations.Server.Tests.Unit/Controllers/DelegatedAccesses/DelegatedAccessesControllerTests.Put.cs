20b6e676d23922888b0ee6f186cc62fb7cc3c86a
Joe Parkes <joe.owens@answerdigital.com>
2024-09-17 15:20:57 +0100

    PutDelegatedAccessAsyncShouldReturnOkWhenDelegatedAccessIsUpdated -> FAIL

---
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Unit/Controllers/DelegatedAccesses/DelegatedAccessesControllerTests.Put.cs
@@ -0,0 +1,38 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using ISL.ReIdentification.Core.Models.Foundations.DelegatedAccesses;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace ISL.ReIdentification.Configurations.Server.Tests.Unit.Controllers.DelegatedAccesses
+{
+    public partial class DelegatedAccessesControllerTests
+    {
+        [Fact]
+        public async Task PutDelegatedAccessAsyncShouldReturnOkWhenDelegatedAccessIsUpdated()
+        {
+            // given
+            DelegatedAccess randomDelegatedAccess = CreateRandomDelegatedAccess();
+            DelegatedAccess inputDelegatedAccess = randomDelegatedAccess;
+            DelegatedAccess storageDelegatedAccess = inputDelegatedAccess.DeepClone();
+            DelegatedAccess expectedDelegatedAccess = storageDelegatedAccess.DeepClone();
+
+            mockDelegatedAccessService
+                .Setup(service => service.ModifyDelegatedAccessAsync(inputDelegatedAccess))
+                .ReturnsAsync(storageDelegatedAccess);
+
+            // when
+            var result = await delegatedAccessesController.PutDelegatedAccessAsync(randomDelegatedAccess);
+
+            // then
+            var createdResult = Assert.IsType<OkObjectResult>(result.Result);
+            createdResult.StatusCode.Should().Be(200);
+            createdResult.Value.Should().BeEquivalentTo(expectedDelegatedAccess);
+        }
+    }
+}
