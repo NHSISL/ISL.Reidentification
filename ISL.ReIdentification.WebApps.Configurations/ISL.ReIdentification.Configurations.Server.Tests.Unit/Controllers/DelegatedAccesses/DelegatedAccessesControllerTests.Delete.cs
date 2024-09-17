b6aa3993ce0cbf558bae4bac5e80c0106f299f2e
Joe Parkes <joe.owens@answerdigital.com>
2024-09-17 17:04:08 +0100

    DeleteDelegatedAccessByIdsAsyncShouldReturnNotFoundWhenDelegatedAccessValidationExceptionOccurs -> FAIL

--- ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Unit/Controllers/DelegatedAccesses/DelegatedAccessesControllerTests.Delete.cs
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Unit/Controllers/DelegatedAccesses/DelegatedAccessesControllerTests.Delete.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using Force.DeepCloner;
 using ISL.ReIdentification.Core.Models.Foundations.DelegatedAccesses;
+using ISL.ReIdentification.Core.Models.Foundations.DelegatedAccesses.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 
@@ -34,6 +35,31 @@ namespace ISL.ReIdentification.Configurations.Server.Tests.Unit.Controllers.Dele
             var actualResult = Assert.IsType<OkObjectResult>(result.Result);
             actualResult.StatusCode.Should().Be(200);
             actualResult.Value.Should().BeEquivalentTo(expectedDelegatedAccess);
+        }
+
+        [Fact]
+        public async Task
+            DeleteDelegatedAccessByIdsAsyncShouldReturnNotFoundWhenDelegatedAccessValidationExceptionOccurs()
+        {
+            // given
+            Guid randomId = Guid.NewGuid();
+            Guid inputId = randomId;
+            var notFoundDelegatedAccessException = new NotFoundDelegatedAccessException(message: inputId.ToString());
+
+            var delegatedAccessValidationException = new DelegatedAccessValidationException(
+                message: GetRandomString(),
+                innerException: notFoundDelegatedAccessException);
+
+            mockDelegatedAccessService
+                .Setup(service => service.RemoveDelegatedAccessByIdAsync(inputId))
+                .ThrowsAsync(delegatedAccessValidationException);
+
+            // when
+            var result = await delegatedAccessesController.DeleteDelegatedAccessByIdAsync(inputId);
+
+            // then
+            var notFoundObjectResult = Assert.IsType<NotFoundObjectResult>(result.Result);
+            notFoundObjectResult.StatusCode.Should().Be(404);
         }
     }
 }
