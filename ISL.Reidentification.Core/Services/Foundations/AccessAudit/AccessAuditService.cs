7f7fcb4281d37c094129a410bd40452f775e2f61
Hassan Raza <h_raza110@outlook.com>
2024-09-17 09:16:46 +0100

    ShouldModifyAccessAuditAsync -> PASS

--- ISL.Reidentification.Core/Services/Foundations/AccessAudit/AccessAuditService.cs
+++ ISL.Reidentification.Core/Services/Foundations/AccessAudit/AccessAuditService.cs
@@ -2,14 +2,13 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
-using System.Linq;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using ISL.ReIdentification.Core.Brokers.DateTimes;
 using ISL.ReIdentification.Core.Brokers.Loggings;
 using ISL.ReIdentification.Core.Brokers.Storages.Sql.ReIdentifications;
 using ISL.ReIdentification.Core.Models.Foundations.AccessAudits;
-using ISL.ReIdentification.Core.Models.Foundations.UserAccesses;
 
 namespace ISL.ReIdentification.Core.Services.Foundations.AccessAudits
 {
@@ -53,8 +52,8 @@ namespace ISL.ReIdentification.Core.Services.Foundations.AccessAudits
                 return maybeAccessAudit;
             });
 
-        public ValueTask<AccessAudit> ModifyAccessAuditAsync(AccessAudit accessAudit) =>
-            throw new NotImplementedException();
+        public async ValueTask<AccessAudit> ModifyAccessAuditAsync(AccessAudit accessAudit) =>
+            await this.reIdentificationStorageBroker.UpdateAccessAuditAsync(accessAudit);
 
         public ValueTask<AccessAudit> RemoveAccessAuditByIdAsync(Guid accessAuditId) =>
             throw new NotImplementedException();
