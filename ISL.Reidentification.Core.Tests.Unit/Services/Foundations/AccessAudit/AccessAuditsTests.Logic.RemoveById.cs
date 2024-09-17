a84ae162015fe172c3b4481f24f18174ff350350
Hassan Raza <h_raza110@outlook.com>
2024-09-17 12:01:15 +0100

    ShouldRemoveAccessAuditByIdAsync -> FAIL

---
+++ ISL.Reidentification.Core.Tests.Unit/Services/Foundations/AccessAudit/AccessAuditsTests.Logic.RemoveById.cs
@@ -0,0 +1,47 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using ISL.ReIdentification.Core.Models.Foundations.AccessAudits;
+using Moq;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.AccessAudits
+{
+    public partial class AccessAuditTests
+    {
+        [Fact]
+        public async Task ShouldRemoveAccessAuditByIdAsync()
+        {
+            // given
+            AccessAudit randomAccessAudit = CreateRandomAccessAudit();
+            AccessAudit inputAccessAudit = randomAccessAudit;
+            Guid inputAccessAuditId = inputAccessAudit.Id;
+            AccessAudit storageAccessAudit = inputAccessAudit;
+            AccessAudit deletedAccessAudit = inputAccessAudit;
+            AccessAudit expectedAccessAudit = deletedAccessAudit.DeepClone();
+
+            this.reIdentificationStorageBroker.Setup(broker =>
+                broker.DeleteAccessAuditAsync(storageAccessAudit))
+                    .ReturnsAsync(deletedAccessAudit);
+
+            // when
+            AccessAudit actualAccessAudit =
+                await this.accessAuditService.RemoveAccessAuditByIdAsync(inputAccessAuditId);
+
+            // then
+            actualAccessAudit.Should().BeEquivalentTo(expectedAccessAudit);
+
+            this.reIdentificationStorageBroker.Verify(broker =>
+                broker.DeleteAccessAuditAsync(inputAccessAudit),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.reIdentificationStorageBroker.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
