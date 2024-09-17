53e7f37eb91b779cbf3617f2c5ce5eed7958b56e
Hassan Raza <h_raza110@outlook.com>
2024-09-17 09:07:24 +0100

    ShouldModifyAccessAuditAsync -> FAIL

---
+++ ISL.Reidentification.Core.Tests.Unit/Services/Foundations/AccessAudit/AccessAuditsTests.Logic.Modify.cs
@@ -0,0 +1,67 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using ISL.ReIdentification.Core.Models.Foundations.AccessAudits;
+using Moq;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.AccessAudits
+{
+    public partial class AccessAuditTests
+    {
+        [Fact]
+        public async Task ShouldModifyAccessAuditAsync()
+        {
+            // given
+            DateTimeOffset randomDateOffset = GetRandomDateTimeOffset();
+
+            AccessAudit randomModifyAccessAudit =
+                CreateRandomModifyAccessAudit(randomDateOffset);
+
+            AccessAudit inputAccessAudit = randomModifyAccessAudit.DeepClone();
+            AccessAudit storageAccessAudit = randomModifyAccessAudit.DeepClone();
+            storageAccessAudit.UpdatedDate = storageAccessAudit.CreatedDate;
+            AccessAudit updatedAccessAudit = inputAccessAudit.DeepClone();
+            AccessAudit expectedAccessAudit = updatedAccessAudit.DeepClone();
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ReturnsAsync(randomDateOffset);
+
+            this.reIdentificationStorageBroker.Setup(broker =>
+                broker.SelectAccessAuditByIdAsync(inputAccessAudit.Id))
+                    .ReturnsAsync(storageAccessAudit);
+
+            this.reIdentificationStorageBroker.Setup(broker =>
+                broker.UpdateAccessAuditAsync(inputAccessAudit))
+                    .ReturnsAsync(updatedAccessAudit);
+
+            // when
+            AccessAudit actualAccessAudit =
+                await this.accessAuditService.ModifyAccessAuditAsync(inputAccessAudit);
+
+            // then
+            actualAccessAudit.Should().BeEquivalentTo(expectedAccessAudit);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.reIdentificationStorageBroker.Verify(broker =>
+                broker.SelectAccessAuditByIdAsync(inputAccessAudit.Id),
+                    Times.Once);
+
+            this.reIdentificationStorageBroker.Verify(broker =>
+                broker.UpdateAccessAuditAsync(inputAccessAudit),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.reIdentificationStorageBroker.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
