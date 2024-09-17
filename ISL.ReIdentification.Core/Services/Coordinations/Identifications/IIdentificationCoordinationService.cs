6e1b69ea8d2a3f502e70b028e07c416b1e8c7404
Christo du Toit <christo@dutoit.co.uk>
2024-09-17 12:33:18 +0100

    CODE RUB:  Stubs for Orchestrations and Coordinations

---
+++ ISL.ReIdentification.Core/Services/Coordinations/Identifications/IIdentificationCoordinationService.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ISL.ReIdentification.Core.Models.Orchestrations.Identifications;
+
+namespace ISL.ReIdentification.Core.Services.Orchestrations.Identifications
+{
+    public interface IIdentificationCoordinationService
+    {
+        ValueTask<List<IdentificationRequest>> ProcessIdentificationRequestsAsync(
+            List<IdentificationRequest> identificationRequests);
+    }
+}
---
+++ ISL.ReIdentification.Core/Services/Foundations/ReIdentifications/IReIdentificationService.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ISL.ReIdentification.Core.Models.Orchestrations.Identifications;
+
+namespace ISL.ReIdentification.Core.Services.Foundations.ReIdentifications
+{
+    public interface IReIdentificationService
+    {
+        ValueTask<List<IdentificationRequest>> ProcessReidentificationRequests(
+            List<IdentificationRequest> identificationRequests);
+    }
+}
