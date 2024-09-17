92a6c9ea77ba0b9b9cc4368573e83e05231d318b
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-09-17 12:23:27 +0100

    GetUserAccessByIdAsyncShouldReturnNotFoundWhenUserAccessValidationExceptionOccurs -> FAIL

--- ISL.Reidentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Unit/Controllers/UserAccesses/UserAccessesControllerTests.Get.cs
+++ ISL.Reidentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Unit/Controllers/UserAccesses/UserAccessesControllerTests.Get.cs
@@ -6,7 +6,9 @@ using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Force.DeepCloner;
+using ISL.Reidentification.Core.Models.Foundations.UserAccesses.Exceptions;
 using ISL.ReIdentification.Core.Models.Foundations.UserAccesses;
+using ISL.ReIdentification.Core.Models.Foundations.UserAccesses.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 
@@ -34,6 +36,30 @@ namespace ISL.ReIdentification.Configurations.Server.Tests.Unit.Controllers.User
             var createdResult = Assert.IsType<OkObjectResult>(result.Result);
             createdResult.StatusCode.Should().Be(200);
             createdResult.Value.Should().BeEquivalentTo(expectedUserAccess);
+        }
+
+        [Fact]
+        public async Task GetUserAccessByIdAsyncShouldReturnNotFoundWhenUserAccessValidationExceptionOccurs()
+        {
+            // given
+            Guid randomId = Guid.NewGuid();
+            Guid inputId = randomId;
+            var notFoundUserAccessException = new NotFoundUserAccessException(message: GetRandomString());
+
+            var userAccessValidationException = new UserAccessValidationException(
+                message: GetRandomString(),
+                innerException: notFoundUserAccessException);
+
+            mockUserAccessService
+                .Setup(service => service.RetrieveUserAccessByIdAsync(inputId))
+                .ThrowsAsync(userAccessValidationException);
+
+            // when
+            var result = await userAccessesController.GetUserAccessByIdAsync(inputId);
+
+            // then
+            var notFoundObjectResult = Assert.IsType<NotFoundObjectResult>(result.Result);
+            notFoundObjectResult.StatusCode.Should().Be(404);
         }
     }
 }
