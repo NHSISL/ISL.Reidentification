adf631bad5cdafa29b91fbe9f0076199aab0a18a
Joe Parkes <joe.owens@answerdigital.com>
2024-09-18 10:34:24 +0100

    Creating RetrievePdsDataByIdAsync on PdsDataService

---
+++ ISL.ReIdentification.Core/Services/Foundations/PdsDatas/IPdsDataService.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using ISL.ReIdentification.Core.Models.Foundations.PdsDatas;
+
+namespace ISL.ReIdentification.Core.Services.Foundations.PdsDatas
+{
+    public interface IPdsDataService
+    {
+        public ValueTask<PdsData> RetrievePdsDataByIdAsync(Guid pdsDataId);
+    }
+}
---
+++ ISL.ReIdentification.Core/Services/Foundations/PdsDatas/PdsDataService.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using ISL.ReIdentification.Core.Brokers.Loggings;
+using ISL.ReIdentification.Core.Brokers.Storages.Sql.Pds;
+using ISL.ReIdentification.Core.Models.Foundations.PdsDatas;
+
+namespace ISL.ReIdentification.Core.Services.Foundations.PdsDatas
+{
+    public partial class PdsDataService : IPdsDataService
+    {
+        private readonly IOdsStorageBroker odsStorageBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public PdsDataService(IOdsStorageBroker odsStorageBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.odsStorageBroker = odsStorageBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public async ValueTask<PdsData> RetrievePdsDataByIdAsync(Guid pdsDataId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
