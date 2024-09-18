d1465490f7b0aab3e00d8635803d4efc03518343
Joe Parkes <joe.owens@answerdigital.com>
2024-09-18 09:02:24 +0100

    Adding PdsDataService

---
+++ ISL.ReIdentification.Core/Services/Foundations/PdsDatas/PdsDataService.cs
@@ -0,0 +1,33 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ISL.ReIdentification.Core.Brokers.Loggings;
+using ISL.ReIdentification.Core.Brokers.Storages.Sql.ReIdentifications;
+using ISL.ReIdentification.Core.Models.Foundations.PdsDatas;
+using ISL.ReIdentification.Core.Services.Foundations.Pds;
+
+namespace ISL.ReIdentification.Core.Services.Foundations.PdsDatas
+{
+    public partial class PdsDataService : IPdsDataService
+    {
+        private readonly IReIdentificationStorageBroker reIdentificationStorageBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public PdsDataService(
+            IReIdentificationStorageBroker reIdentificationStorageBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.reIdentificationStorageBroker = reIdentificationStorageBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<IQueryable<PdsData>> RetrieveAllPdsDataAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
