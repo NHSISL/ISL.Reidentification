235c32af62f56c6a91e2027ce88dc08d519f4844
Joe Parkes <joe.owens@answerdigital.com>
2024-09-18 14:21:10 +0100

    ShouldPostDelegatedAccessAsync -> PASS

---
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Acceptance/Apis/DelegatedAccessesApiTests.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using ISL.ReIdentification.Configurations.Server.Tests.Acceptance.Brokers;
+using ISL.ReIdentification.Configurations.Server.Tests.Acceptance.Models.DelegatedAccesses;
+using Tynamix.ObjectFiller;
+
+namespace ISL.ReIdentification.Configurations.Server.Tests.Acceptance.Apis
+{
+    [Collection(nameof(ApiTestCollection))]
+    public partial class DelegatedAccessesApiTests
+    {
+        private readonly ApiBroker apiBroker;
+
+        public DelegatedAccessesApiTests(ApiBroker apiBroker) =>
+            this.apiBroker = apiBroker;
+
+        private static DelegatedAccess CreateRandomDelegatedAccess() =>
+           CreateRandomDelegatedAccessFiller().Create();
+
+        private static string GetRandomStringWithLengthOf(int length) =>
+            new MnemonicString(wordCount: 1, wordMinLength: length, wordMaxLength: length).GetValue();
+
+        private static Filler<DelegatedAccess> CreateRandomDelegatedAccessFiller()
+        {
+            string user = Guid.NewGuid().ToString();
+            DateTime now = DateTime.UtcNow;
+            var filler = new Filler<DelegatedAccess>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(now)
+                .OnProperty(delegatedAccess => delegatedAccess.RequesterEmail).Use(() => GetRandomStringWithLengthOf(320))
+                .OnProperty(delegatedAccess => delegatedAccess.RecipientEmail).Use(() => GetRandomStringWithLengthOf(320))
+                .OnProperty(delegatedAccess => delegatedAccess.IdentifierColumn).Use(() => GetRandomStringWithLengthOf(10))
+                .OnProperty(delegatedAccess => delegatedAccess.CreatedDate).Use(now)
+                .OnProperty(delegatedAccess => delegatedAccess.CreatedBy).Use(user)
+                .OnProperty(delegatedAccess => delegatedAccess.UpdatedDate).Use(now)
+                .OnProperty(delegatedAccess => delegatedAccess.UpdatedBy).Use(user);
+
+            return filler;
+        }
+    }
+}
---
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Acceptance/Brokers/ApiBroker.DelegatedAccesses.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using ISL.ReIdentification.Configurations.Server.Tests.Acceptance.Models.DelegatedAccesses;
+
+namespace ISL.ReIdentification.Configurations.Server.Tests.Acceptance.Brokers
+{
+    public partial class ApiBroker
+    {
+        private const string DelegatedAccessesRelativeUrl = "api/delegatedAccesses";
+
+        public async ValueTask<DelegatedAccess> PostDelegatedAccessAsync(DelegatedAccess delegatedAccess) =>
+            await this.apiFactoryClient.PostContentAsync(DelegatedAccessesRelativeUrl, delegatedAccess);
+
+        public async ValueTask<DelegatedAccess> GetDelegatedAccessByIdAsync(Guid delegatedAccessId) =>
+            await this.apiFactoryClient
+                .GetContentAsync<DelegatedAccess>($"{DelegatedAccessesRelativeUrl}/{delegatedAccessId}");
+
+        public async ValueTask<DelegatedAccess> DeleteDelegatedAccessByIdAsync(Guid delegatedAccessId) =>
+            await this.apiFactoryClient.DeleteContentAsync<DelegatedAccess>($"{DelegatedAccessesRelativeUrl}/{delegatedAccessId}");
+    }
+}
