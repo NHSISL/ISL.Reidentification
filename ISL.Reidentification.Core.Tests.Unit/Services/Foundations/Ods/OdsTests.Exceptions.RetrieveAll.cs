5fe3efa70c822539a8273f3826075073d9ded350
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-09-18 10:19:11 +0100

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ ISL.Reidentification.Core.Tests.Unit/Services/Foundations/Ods/OdsTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,60 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ISL.ReIdentification.Core.Models.Foundations.OdsDatas;
+using ISL.ReIdentification.Core.Models.Foundations.OdsDatas.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.Ods
+{
+    public partial class OdsTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            SqlException sqlException = CreateSqlException();
+
+            var failedStorageOdsDataException = new FailedStorageOdsDataException(
+                message: "Failed ODS data storage error occurred, contact support.",
+                innerException: sqlException);
+
+            var expectedOdsDataDependencyException = new OdsDataDependencyException(
+                message: "OdsData dependency error occurred, contact support.",
+                innerException: failedStorageOdsDataException);
+
+            this.odsStorageBroker.Setup(broker =>
+                broker.SelectAllOdsDatasAsync())
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<IQueryable<OdsData>> retrieveAllOdsDatasTask =
+                this.odsService.RetrieveAllOdsDatasAsync();
+
+            OdsDataDependencyException actualOdsDataDependencyException =
+                await Assert.ThrowsAsync<OdsDataDependencyException>(
+                    retrieveAllOdsDatasTask.AsTask);
+
+            // then
+            actualOdsDataDependencyException.Should().BeEquivalentTo(expectedOdsDataDependencyException);
+
+            this.odsStorageBroker.Verify(broker =>
+                broker.SelectAllOdsDatasAsync(),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedOdsDataDependencyException))),
+                        Times.Once());
+
+            this.odsStorageBroker.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- ISL.Reidentification.Core.Tests.Unit/Services/Foundations/Ods/OdsTests.cs
+++ ISL.Reidentification.Core.Tests.Unit/Services/Foundations/Ods/OdsTests.cs
@@ -2,14 +2,19 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using ISL.ReIdentification.Core.Brokers.DateTimes;
 using ISL.ReIdentification.Core.Brokers.Loggings;
 using ISL.ReIdentification.Core.Brokers.Storages.Sql.Ods;
 using ISL.ReIdentification.Core.Models.Foundations.OdsDatas;
 using ISL.ReIdentification.Core.Services.Foundations.Ods;
+using Microsoft.Data.SqlClient;
 using Moq;
 using Tynamix.ObjectFiller;
+using Xeptions;
 
 namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.Ods
 {
@@ -42,10 +47,20 @@ namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.Ods
                 .AsQueryable();
         }
 
+        private SqlException CreateSqlException() =>
+            (SqlException)RuntimeHelpers.GetUninitializedObject(type: typeof(SqlException));
+
         private static int GetRandomNumber() =>
             new IntRange(max: 15, min: 2).GetValue();
 
         private static Filler<OdsData> CreateOdsDataFiller() =>
             new Filler<OdsData>();
+
+        private static Expression<Func<Xeption, bool>> SameExceptionAs(
+            Xeption expectedException)
+        {
+            return actualException =>
+                actualException.SameExceptionAs(expectedException);
+        }
     }
 }
