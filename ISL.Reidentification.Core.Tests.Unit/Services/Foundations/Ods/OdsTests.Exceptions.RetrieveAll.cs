71247aa7f87b59d7941f41c7caacdc325e51367f
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-09-18 11:10:23 +0100

    ShouldThrowServiceExceptionOnRetrieveAllWhenServiceErrorOccursAndLogItAsync -> FAIL

--- ISL.Reidentification.Core.Tests.Unit/Services/Foundations/Ods/OdsTests.Exceptions.RetrieveAll.cs
+++ ISL.Reidentification.Core.Tests.Unit/Services/Foundations/Ods/OdsTests.Exceptions.RetrieveAll.cs
@@ -2,6 +2,7 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -55,6 +56,50 @@ namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.Ods
             this.odsStorageBroker.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRetrieveAllWhenServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            Exception serviceException = new Exception();
+
+            var failedServiceOdsDataException = new FailedServiceOdsDataException(
+                message: "Failed service ODS data error occurred, contact support.",
+                innerException: serviceException);
+
+            var expectedOdsDataServiceException = new OdsDataServiceException(
+                message: "Service error occurred, contact support.",
+                innerException: failedServiceOdsDataException);
+
+            this.odsStorageBroker.Setup(broker =>
+                broker.SelectAllOdsDatasAsync())
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<IQueryable<OdsData>> retrieveAllOdsDatasTask =
+                this.odsService.RetrieveAllOdsDatasAsync();
+
+            OdsDataDependencyException actualOdsDataDependencyException =
+                await Assert.ThrowsAsync<OdsDataDependencyException>(
+                    retrieveAllOdsDatasTask.AsTask);
+
+            // then
+            actualOdsDataDependencyException.Should().BeEquivalentTo(expectedOdsDataServiceException);
+
+            this.odsStorageBroker.Verify(broker =>
+                broker.SelectAllOdsDatasAsync(),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedOdsDataServiceException))),
+                        Times.Once());
+
+            this.odsStorageBroker.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+
         }
     }
 }
