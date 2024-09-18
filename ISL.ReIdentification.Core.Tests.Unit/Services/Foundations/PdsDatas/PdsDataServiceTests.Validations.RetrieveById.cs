d37651fa85d5537d19262604821a4ec62b3c06ba
Joe Parkes <joe.owens@answerdigital.com>
2024-09-18 10:46:55 +0100

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Foundations/PdsDatas/PdsDataServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,61 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ISL.ReIdentification.Core.Models.Foundations.PdsDatas;
+using ISL.ReIdentification.Core.Models.Foundations.PdsDatas.Exceptions;
+using Moq;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.PdsDatas
+{
+    public partial class PdsDataServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            var invalidPdsDataId = Guid.Empty;
+
+            var invalidPdsDataException =
+                new InvalidPdsDataException(
+                    message: "Invalid pds data. Please correct the errors and try again.");
+
+            invalidPdsDataException.AddData(
+                key: nameof(PdsData.Id),
+                values: "Id is invalid");
+
+            var expectedPdsDataValidationException =
+                new PdsDataValidationException(
+                    message: "PdsData validation error occurred, please fix errors and try again.",
+                    innerException: invalidPdsDataException);
+
+            // when
+            ValueTask<PdsData> retrievePdsDataByIdTask =
+                this.pdsDataService.RetrievePdsDataByIdAsync(invalidPdsDataId);
+
+            PdsDataValidationException actualPdsDataValidationException =
+                await Assert.ThrowsAsync<PdsDataValidationException>(
+                    retrievePdsDataByIdTask.AsTask);
+
+            // then
+            actualPdsDataValidationException.Should()
+                .BeEquivalentTo(expectedPdsDataValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedPdsDataValidationException))),
+                        Times.Once);
+
+            this.odsStorageBroker.Verify(broker =>
+                broker.SelectPdsDataByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.odsStorageBroker.VerifyNoOtherCalls();
+        }
+
+    }
+}
