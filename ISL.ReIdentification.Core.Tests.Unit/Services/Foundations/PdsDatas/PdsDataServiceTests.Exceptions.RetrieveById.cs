9debf02922b539dce093a71418556a87aa07f2f9
Joe Parkes <joe.owens@answerdigital.com>
2024-09-18 11:41:45 +0100

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Foundations/PdsDatas/PdsDataServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,63 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ISL.ReIdentification.Core.Models.Foundations.PdsDatas;
+using ISL.ReIdentification.Core.Models.Foundations.PdsDatas.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.PdsDatas
+{
+    public partial class PdsDataServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            SqlException sqlException = CreateSqlException();
+
+            var failedStoragePdsDataException =
+                new FailedStoragePdsDataException(
+                    message: "Failed pds data storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedPdsDataDependencyException =
+                new PdsDataDependencyException(
+                    message: "PdsData dependency error occurred, contact support.",
+                    innerException: failedStoragePdsDataException);
+
+            this.odsStorageBroker.Setup(broker =>
+                broker.SelectPdsDataByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<PdsData> retrievePdsDataByIdTask =
+                this.pdsDataService.RetrievePdsDataByIdAsync(someId);
+
+            PdsDataDependencyException actualPdsDataDependencyException =
+                await Assert.ThrowsAsync<PdsDataDependencyException>(
+                    retrievePdsDataByIdTask.AsTask);
+
+            // then
+            actualPdsDataDependencyException.Should()
+                .BeEquivalentTo(expectedPdsDataDependencyException);
+
+            this.odsStorageBroker.Verify(broker =>
+                broker.SelectPdsDataByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedPdsDataDependencyException))),
+                        Times.Once);
+
+            this.odsStorageBroker.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ ISL.ReIdentification.Core/Models/Foundations/PdsDatas/Exceptions/PdsDataDependencyException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using Xeptions;
+
+namespace ISL.ReIdentification.Core.Models.Foundations.PdsDatas.Exceptions
+{
+    public class PdsDataDependencyException : Xeption
+    {
+        public PdsDataDependencyException(string message, Xeption innerException)
+            : base(message, innerException)
+        { }
+    }
+}
