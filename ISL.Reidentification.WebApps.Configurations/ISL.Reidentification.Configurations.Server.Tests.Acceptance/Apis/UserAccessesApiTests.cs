a513a20d276d5da405a59ad0c55ea6bfe1cbe50e
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-09-19 10:16:33 +0100

    ShouldPutUserAccessAsync -> FAIL

--- ISL.Reidentification.WebApps.Configurations/ISL.Reidentification.Configurations.Server.Tests.Acceptance/Apis/UserAccessesApiTests.cs
+++ ISL.Reidentification.WebApps.Configurations/ISL.Reidentification.Configurations.Server.Tests.Acceptance/Apis/UserAccessesApiTests.cs
@@ -22,6 +22,9 @@ namespace ISL.ReIdentification.Configurations.Server.Tests.Acceptance.Apis
         private static int GetRandomNumber() =>
             new IntRange(min: 2, max: 10).GetValue();
 
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
 
         private static UserAccess CreateRandomUserAccess() =>
             CreateRandomUserAccessFiller().Create();
@@ -35,12 +38,29 @@ namespace ISL.ReIdentification.Configurations.Server.Tests.Acceptance.Apis
             filler.Setup()
                 .OnType<DateTimeOffset>().Use(now)
                 .OnType<DateTimeOffset?>().Use(now)
-                .OnProperty(lookup => lookup.CreatedDate).Use(now)
-                .OnProperty(lookup => lookup.CreatedBy).Use(user)
-                .OnProperty(lookup => lookup.UpdatedDate).Use(now)
-                .OnProperty(lookup => lookup.UpdatedBy).Use(user);
+                .OnProperty(userAccess => userAccess.CreatedDate).Use(now)
+                .OnProperty(userAccess => userAccess.CreatedBy).Use(user)
+                .OnProperty(userAccess => userAccess.UpdatedDate).Use(now)
+                .OnProperty(userAccess => userAccess.UpdatedBy).Use(user);
 
             return filler;
+        }
+
+        private static UserAccess UpdateUserAccess(UserAccess inputUserAccess)
+        {
+
+            DateTimeOffset now = DateTimeOffset.UtcNow;
+            var filler = new Filler<UserAccess>();
+
+            filler.Setup()
+                .OnProperty(userAccess => userAccess.Id).Use(inputUserAccess.Id)
+                .OnType<DateTimeOffset>().Use(GetRandomDateTimeOffset())
+                .OnType<DateTimeOffset?>().Use(GetRandomDateTimeOffset())
+                .OnProperty(userAccess => userAccess.CreatedDate).Use(inputUserAccess.CreatedDate)
+                .OnProperty(userAccess => userAccess.CreatedBy).Use(inputUserAccess.CreatedBy)
+                .OnProperty(userAccess => userAccess.UpdatedDate).Use(now);
+
+            return filler.Create();
         }
 
         private async ValueTask<List<UserAccess>> PostRandomUserAccesses()
