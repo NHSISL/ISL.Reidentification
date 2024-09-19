fbd171a05af1f25ab09036169ce181fa51d5ce9f
Joe Parkes <joe.owens@answerdigital.com>
2024-09-19 10:37:01 +0100

    GetAllPdsDataAsyncShouldReturnItems -> FAIL

---
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Unit/Controllers/PdsDatas/PdsDataControllerTests.GetAll.cs
@@ -0,0 +1,38 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using ISL.ReIdentification.Core.Models.Foundations.PdsDatas;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace ISL.ReIdentification.Configurations.Server.Tests.Unit.Controllers.PdsDatas
+{
+    public partial class PdsDataControllerTests
+    {
+        [Fact]
+        public async Task GetAllPdsDataAsyncShouldReturnItems()
+        {
+            // given
+            IQueryable<PdsData> randomPdsData = CreateRandomPdsDatas();
+            IQueryable<PdsData> storagePdsData = randomPdsData.DeepClone();
+            IQueryable<PdsData> expectedPdsData = storagePdsData.DeepClone();
+
+            this.mockPdsDataService
+                .Setup(service => service.RetrieveAllPdsDataAsync())
+                .ReturnsAsync(storagePdsData);
+
+            // when
+            var result = await this.pdsDataController.Get();
+
+            // then
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            okResult.StatusCode.Should().Be(200);
+            okResult.Value.Should().BeEquivalentTo(expectedPdsData);
+        }
+    }
+}
---
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Unit/Controllers/PdsDatas/PdsDataControllerTests.cs
@@ -0,0 +1,44 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Linq;
+using ISL.ReIdentification.Configurations.Server.Controllers;
+using ISL.ReIdentification.Core.Models.Foundations.PdsDatas;
+using ISL.ReIdentification.Core.Services.Foundations.PdsDatas;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace ISL.ReIdentification.Configurations.Server.Tests.Unit.Controllers.PdsDatas
+{
+    public partial class PdsDataControllerTests
+    {
+        private readonly Mock<IPdsDataService> mockPdsDataService;
+        private readonly PdsDataController pdsDataController;
+
+        public PdsDataControllerTests()
+        {
+            mockPdsDataService = new Mock<IPdsDataService>();
+            pdsDataController = new PdsDataController(mockPdsDataService.Object);
+        }
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static IQueryable<PdsData> CreateRandomPdsDatas()
+        {
+            return CreatePdsDataFiller()
+                .Create(count: GetRandomNumber())
+                    .AsQueryable();
+        }
+
+        private static Filler<PdsData> CreatePdsDataFiller()
+        {
+            var filler = new Filler<PdsData>();
+
+            filler.Setup();
+
+            return filler;
+        }
+    }
+}
