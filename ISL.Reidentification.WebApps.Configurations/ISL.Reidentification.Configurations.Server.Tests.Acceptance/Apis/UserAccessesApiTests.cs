c389c265225c4532b3bf8fa0c31bbcced9c9b8b7
MichaelOSullivanAnswer <116073025+MichaelOSullivanAnswer@users.noreply.github.com>
2024-09-19 10:22:38 +0100

    CODE RUB: Code cleanup

--- ISL.Reidentification.WebApps.Configurations/ISL.Reidentification.Configurations.Server.Tests.Acceptance/Apis/UserAccessesApiTests.cs
+++ ISL.Reidentification.WebApps.Configurations/ISL.Reidentification.Configurations.Server.Tests.Acceptance/Apis/UserAccessesApiTests.cs
@@ -22,7 +22,6 @@ namespace ISL.ReIdentification.Configurations.Server.Tests.Acceptance.Apis
         private static int GetRandomNumber() =>
             new IntRange(min: 2, max: 10).GetValue();
 
-
         private static UserAccess CreateRandomUserAccess() =>
             CreateRandomUserAccessFiller().Create();
 
