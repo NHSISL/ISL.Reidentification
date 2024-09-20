ebb61ca3f022795297279dcabf68213681669d60
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-09-20 09:24:44 +0100

    GetOdsDataByIdAsyncShouldReturnNotFoundWhenOdsDataValidationExceptionOccurs -> FAIL

--- ISL.Reidentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Unit/Controllers/OdsDatas/OdsDataControllerTests.Get.cs
+++ ISL.Reidentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Unit/Controllers/OdsDatas/OdsDataControllerTests.Get.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using Force.DeepCloner;
 using ISL.ReIdentification.Core.Models.Foundations.OdsDatas;
+using ISL.ReIdentification.Core.Models.Foundations.OdsDatas.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 
@@ -34,6 +35,30 @@ namespace ISL.ReIdentification.Configurations.Server.Tests.Unit.Controllers.OdsD
             var createdResult = Assert.IsType<OkObjectResult>(result.Result);
             createdResult.StatusCode.Should().Be(200);
             createdResult.Value.Should().BeEquivalentTo(expectedOdsData);
+        }
+
+        [Fact]
+        public async Task GetOdsDataByIdAsyncShouldReturnNotFoundWhenOdsDataValidationExceptionOccurs()
+        {
+            // given
+            Guid randomId = Guid.NewGuid();
+            Guid inputId = randomId;
+            var notFoundOdsDataException = new NotFoundOdsDataException(odsDataId: inputId);
+
+            var odsDataValidationException = new OdsDataValidationException(
+                message: GetRandomString(),
+                innerException: notFoundOdsDataException);
+
+            this.odsDataServiceMock
+                .Setup(service => service.RetrieveOdsDataByIdAsync(inputId))
+                .ThrowsAsync(odsDataValidationException);
+
+            // when
+            var result = await this.odsDataController.GetOdsDataByIdAsync(inputId);
+
+            // then
+            var notFoundObjectResult = Assert.IsType<NotFoundObjectResult>(result.Result);
+            notFoundObjectResult.StatusCode.Should().Be(404);
         }
     }
 }
