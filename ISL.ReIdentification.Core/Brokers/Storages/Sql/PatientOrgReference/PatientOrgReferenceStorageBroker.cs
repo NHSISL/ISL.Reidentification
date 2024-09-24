e603c64296a55b6d6acd4d62b05dd04758192545
Joe Parkes <joe.owens@answerdigital.com>
2024-09-24 15:09:29 +0100

    DATA: Adding new fields to OdsData model and adding config to enforce not null and string length

--- ISL.ReIdentification.Core/Brokers/Storages/Sql/PatientOrgReference/PatientOrgReferenceStorageBroker.cs
+++ ISL.ReIdentification.Core/Brokers/Storages/Sql/PatientOrgReference/PatientOrgReferenceStorageBroker.cs
@@ -5,6 +5,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using EFxceptions;
+using ISL.ReIdentification.Core.Models.Foundations.OdsDatas;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -31,7 +32,9 @@ namespace ISL.ReIdentification.Core.Brokers.Storages.Sql.PatientOrgReference
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        { }
+        {
+            AddOdsDataConfigurations(modelBuilder.Entity<OdsData>());
+        }
 
         private async ValueTask<T> InsertAsync<T>(T @object)
         {
