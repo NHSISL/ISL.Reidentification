4532c6f885f652a79422a2d942a9b95f95994652
Joe Parkes <joe.owens@answerdigital.com>
2024-10-01 15:52:48 +0100

    ShouldValidateAccessForIdentificationRequests -> FAIL

---
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Orchestrations/Accesses/AccessOrchestrationServiceTests.Logic.ValidateAccessForIdentificationRequests.cs
@@ -0,0 +1,61 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ISL.ReIdentification.Core.Models.Orchestrations.Accesses;
+using ISL.ReIdentification.Core.Services.Orchestrations.Accesses;
+using Moq;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Orchestrations.Accesses
+{
+    public partial class AccessOrchestrationServiceTests
+    {
+        [Fact]
+        public async Task ShouldValidateAccessForIdentificationRequests()
+        {
+            // given
+            var accessOrchestrationServiceMock = new Mock<AccessOrchestrationService> { CallBase = true };
+            string userEmail = GetRandomStringWithLength(10);
+            string inputUserEmail = userEmail;
+            string identifier = GetRandomStringWithLength(5);
+            string inputIdentifier = identifier;
+            AccessRequest accessRequest = CreateRandomAccessRequest();
+            string userOrganisation = GetRandomStringWithLength(5);
+
+            List<string> userOrganisations =
+                new List<string> { userOrganisation };
+
+            bool userHasAccessToPatientResult = true;
+
+            accessOrchestrationServiceMock.Setup(service =>
+                service.GetOrganisationsForUserAsync(inputUserEmail))
+                    .ReturnsAsync(userOrganisations);
+
+            accessOrchestrationServiceMock.Setup(service =>
+                service.UserHasAccessToPatientAsync(inputIdentifier, userOrganisations))
+                    .ReturnsAsync(userHasAccessToPatientResult);
+
+            AccessRequest expectedAccessRequest = accessRequest;
+
+            // when
+            AccessRequest actualAccessRequest =
+                await this.accessOrchestrationService.ValidateAccessForIdentificationRequestsAsync(accessRequest);
+
+            // then
+            actualAccessRequest.Should().Be(expectedAccessRequest);
+
+            accessOrchestrationServiceMock.Verify(service =>
+                service.GetOrganisationsForUserAsync(inputUserEmail),
+                    Times.Once);
+
+            accessOrchestrationServiceMock.Verify(service =>
+                service.UserHasAccessToPatientAsync(inputIdentifier, userOrganisations),
+                    Times.Once);
+
+            accessOrchestrationServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- ISL.ReIdentification.Core.Tests.Unit/Services/Orchestrations/Accesses/AccessOrchestrationServiceTests.cs
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Orchestrations/Accesses/AccessOrchestrationServiceTests.cs
@@ -4,14 +4,18 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using ISL.ReIdentification.Core.Brokers.DateTimes;
 using ISL.ReIdentification.Core.Brokers.Loggings;
 using ISL.ReIdentification.Core.Brokers.Storages.Sql.PatientOrgReference;
 using ISL.ReIdentification.Core.Brokers.Storages.Sql.ReIdentifications;
+using ISL.ReIdentification.Core.Models.Foundations.DelegatedAccesses;
 using ISL.ReIdentification.Core.Models.Foundations.OdsDatas;
 using ISL.ReIdentification.Core.Models.Foundations.PdsDatas;
+using ISL.ReIdentification.Core.Models.Foundations.ReIdentifications;
 using ISL.ReIdentification.Core.Models.Foundations.UserAccesses;
+using ISL.ReIdentification.Core.Models.Orchestrations.Accesses;
 using ISL.ReIdentification.Core.Services.Orchestrations.Accesses;
 using Moq;
 using Tynamix.ObjectFiller;
@@ -81,6 +85,29 @@ namespace ISL.ReIdentification.Core.Tests.Unit.Services.Orchestrations.Accesses
         private static UserAccess CreateRandomUserAccess(DateTimeOffset dateTimeOffset) =>
             CreateUserAccessesFiller(dateTimeOffset).Create();
 
+        private static List<IdentificationItem> CreateRandomIdentificationItems()
+        {
+            return CreateIdentificationItemFiller()
+                .Create(GetRandomNumber())
+                .ToList();
+        }
+
+        private static List<IdentificationRequest> CreateRandomIdentificationRequests()
+        {
+            return CreateIdentificationRequestFiller()
+                .Create(GetRandomNumber())
+                .ToList();
+        }
+
+        private static AccessRequest CreateRandomAccessRequest() =>
+            CreateAccessRequestFiller().Create();
+
+        private static DelegatedAccess CreateRandomDelegatedAccess() =>
+            CreateRandomDelegatedAccess(dateTimeOffset: GetRandomDateTimeOffset());
+
+        private static DelegatedAccess CreateRandomDelegatedAccess(DateTimeOffset dateTimeOffset) =>
+            CreateDelegatedAccessesFiller(dateTimeOffset).Create();
+
         private static int GetRandomNumber() =>
             new IntRange(max: 15, min: 2).GetValue();
 
@@ -123,6 +150,58 @@ namespace ISL.ReIdentification.Core.Tests.Unit.Services.Orchestrations.Accesses
                 .OnType<DateTimeOffset?>().Use(dateTimeOffset)
                 .OnProperty(userAccess => userAccess.CreatedBy).Use(user)
                 .OnProperty(userAccess => userAccess.UpdatedBy).Use(user);
+
+            return filler;
+        }
+
+        private static Filler<IdentificationItem> CreateIdentificationItemFiller()
+        {
+            var filler = new Filler<IdentificationItem>();
+
+            filler.Setup()
+                .OnProperty(identificationItem => identificationItem.HasAccess).Use(false);
+
+            return filler;
+        }
+
+        private static Filler<IdentificationRequest> CreateIdentificationRequestFiller()
+        {
+            var filler = new Filler<IdentificationRequest>();
+
+            filler.Setup()
+                .OnProperty(identificationRequest => identificationRequest.IdentificationItems)
+                    .Use(CreateRandomIdentificationItems());
+
+            return filler;
+        }
+
+        private static Filler<AccessRequest> CreateAccessRequestFiller()
+        {
+            var filler = new Filler<AccessRequest>();
+
+            filler.Setup()
+                .OnProperty(accessRequest => accessRequest.IdentificationRequests)
+                    .Use(CreateRandomIdentificationRequests())
+                .OnProperty(accessRequest => accessRequest.DelegatedAccessRequest)
+                    .Use(CreateRandomDelegatedAccess());
+
+            return filler;
+        }
+
+        private static Filler<DelegatedAccess> CreateDelegatedAccessesFiller(DateTimeOffset dateTimeOffset)
+        {
+            string user = Guid.NewGuid().ToString();
+            var filler = new Filler<DelegatedAccess>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset)
+
+                .OnProperty(delegatedAccess => delegatedAccess.IdentifierColumn)
+                    .Use(() => GetRandomStringWithLength(10))
+
+                .OnProperty(delegatedAccess => delegatedAccess.CreatedBy).Use(user)
+                .OnProperty(delegatedAccess => delegatedAccess.UpdatedBy).Use(user);
 
             return filler;
         }
