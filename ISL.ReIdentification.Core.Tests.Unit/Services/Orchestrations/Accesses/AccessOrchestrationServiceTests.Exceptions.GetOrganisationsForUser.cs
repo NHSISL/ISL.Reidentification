56d32e0d90e0adc62cd827462c53b13285673c5f
Joe Parkes <joe.owens@answerdigital.com>
2024-10-01 11:12:06 +0100

    ShouldThrowServiceExceptionOnModifyIfServiceErrorOccurredAndLogItAsync -> FAIL

---
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Orchestrations/Accesses/AccessOrchestrationServiceTests.Exceptions.GetOrganisationsForUser.cs
@@ -0,0 +1,72 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ISL.ReIdentification.Core.Models.Orchestrations.Accesses.Exceptions;
+using Moq;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Orchestrations.Accesses
+{
+    public partial class AccessOrchestrationServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnModifyIfServiceErrorOccurredAndLogItAsync()
+        {
+            // given
+            string someUserEmail = GetRandomStringWithLength(10);
+            var serviceException = new Exception();
+
+            var failedServiceAccessOrchestrationException =
+                new FailedServiceAccessOrchestrationException(
+                    message: "Failed service access orchestration error occurred, contact support.",
+                    innerException: serviceException);
+
+            var expectedAccessOrchestrationServiceException =
+                new AccessOrchestrationServiceException(
+                    message: "Service error occurred, contact support.",
+                    innerException: failedServiceAccessOrchestrationException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<List<string>> getOrganisationsForUserTask =
+                this.accessOrchestrationService.GetOrganisationsForUserAsync(someUserEmail);
+
+            AccessOrchestrationServiceException actualAccessOrchestrationServiceException =
+                await Assert.ThrowsAsync<AccessOrchestrationServiceException>(
+                    testCode: getOrganisationsForUserTask.AsTask);
+
+            // then
+            actualAccessOrchestrationServiceException.Should().BeEquivalentTo(
+                expectedAccessOrchestrationServiceException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedAccessOrchestrationServiceException))),
+                        Times.Once);
+
+            this.reIdentificationStorageBrokerMock.Verify(broker =>
+                broker.SelectAllUserAccessesAsync(),
+                    Times.Never);
+
+            this.patientOrgReferenceStorageBrokerMock.Verify(broker =>
+                broker.SelectAllOdsDatasAsync(),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.reIdentificationStorageBrokerMock.VerifyNoOtherCalls();
+            this.patientOrgReferenceStorageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
