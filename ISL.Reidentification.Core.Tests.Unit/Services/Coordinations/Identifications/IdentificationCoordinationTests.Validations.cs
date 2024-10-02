937c9e021bf9dd54d28906aaec73d69bd985a8c6
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-10-02 15:56:46 +0100

    ShouldThrowValidationExceptionOnProcessWhenIdentificationRequestIsNullAndLogItAsync -> FAIL

--- ISL.Reidentification.Core.Tests.Unit/Services/Coordinations/Identifications/IdentificationCoordinationTests.Validations.cs
+++ ISL.Reidentification.Core.Tests.Unit/Services/Coordinations/Identifications/IdentificationCoordinationTests.Validations.cs
@@ -5,6 +5,7 @@
 using System.Threading.Tasks;
 using FluentAssertions;
 using ISL.ReIdentification.Core.Models.Coordinations.Identifications.Exceptions;
+using ISL.ReIdentification.Core.Models.Foundations.ReIdentifications.Exceptions;
 using ISL.ReIdentification.Core.Models.Orchestrations.Accesses;
 using ISL.ReIdentification.Core.Models.Orchestrations.Accesses.Exceptions;
 using Moq;
@@ -18,7 +19,6 @@ namespace ISL.ReIdentification.Core.Tests.Unit.Services.Coordinations.Identifica
         {
             // given
             AccessRequest nullAccessRequest = null;
-            AccessRequest randomAccessRequest = CreateRandomAccessRequest();
 
             var nullAccessRequestException =
                 new NullAccessRequestException(message: "Access request is null.");
@@ -36,6 +36,48 @@ namespace ISL.ReIdentification.Core.Tests.Unit.Services.Coordinations.Identifica
             // when
             ValueTask<AccessRequest> accessRequestTask =
                 this.identificationCoordinationService.ProcessIdentificationRequestsAsync(nullAccessRequest);
+
+            IdentificationCoordinationValidationException actualIdentificationCoordinationValidationException =
+                await Assert.ThrowsAsync<IdentificationCoordinationValidationException>(accessRequestTask.AsTask);
+
+            // then
+            actualIdentificationCoordinationValidationException
+                .Should().BeEquivalentTo(expectedIdentificationCoordinationValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogErrorAsync(It.Is(SameExceptionAs(
+                   expectedIdentificationCoordinationValidationException))),
+                       Times.Once);
+
+            this.accessOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.identificationOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnProcessWhenIdentificationRequestIsNullAndLogItAsync()
+        {
+            // given
+            AccessRequest randomAccessRequest = CreateRandomAccessRequest();
+            AccessRequest inputAccessRequest = randomAccessRequest;
+            inputAccessRequest.IdentificationRequest = null;
+
+            var nullIdentificationRequestException =
+                new NullIdentificationRequestException(message: "Identification request is null.");
+
+            var expectedIdentificationCoordinationValidationException =
+                new IdentificationCoordinationValidationException(
+                    message: "Identification coordination validation error occurred, " +
+                        "fix the errors and try again.",
+                    innerException: nullIdentificationRequestException);
+
+            this.accessOrchestrationServiceMock.Setup(service =>
+                service.ValidateAccessForIdentificationRequestsAsync(inputAccessRequest))
+                    .ThrowsAsync(nullIdentificationRequestException);
+
+            // when
+            ValueTask<AccessRequest> accessRequestTask =
+                this.identificationCoordinationService.ProcessIdentificationRequestsAsync(inputAccessRequest);
 
             IdentificationCoordinationValidationException actualIdentificationCoordinationValidationException =
                 await Assert.ThrowsAsync<IdentificationCoordinationValidationException>(accessRequestTask.AsTask);
