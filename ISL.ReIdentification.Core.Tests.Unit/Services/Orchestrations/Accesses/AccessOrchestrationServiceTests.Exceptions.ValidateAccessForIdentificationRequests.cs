27002ead0e21541a1aa518d740bd8e0cfc4a0203
Joe Parkes <joe.owens@answerdigital.com>
2024-10-02 15:56:30 +0100

    ShouldThrowServiceExceptionOnAddIfServiceErrorOccurredAndLogItAsync -> FAIL

--- ISL.ReIdentification.Core.Tests.Unit/Services/Orchestrations/Accesses/AccessOrchestrationServiceTests.Exceptions.ValidateAccessForIdentificationRequests.cs
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Orchestrations/Accesses/AccessOrchestrationServiceTests.Exceptions.ValidateAccessForIdentificationRequests.cs
@@ -2,6 +2,7 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using ISL.ReIdentification.Core.Models.Orchestrations.Accesses;
@@ -110,6 +111,60 @@ namespace ISL.ReIdentification.Core.Tests.Unit.Services.Orchestrations.Accesses
             this.loggingBrokerMock.Verify(broker =>
                broker.LogErrorAsync(It.Is(SameExceptionAs(
                    expectedAccessOrchestrationDependencyException))),
+                       Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.reIdentificationStorageBrokerMock.VerifyNoOtherCalls();
+            this.patientOrgReferenceStorageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnAddIfServiceErrorOccurredAndLogItAsync()
+        {
+            var accessOrchestrationServiceMock = new Mock<AccessOrchestrationService>
+                (this.dateTimeBrokerMock.Object,
+                this.reIdentificationStorageBrokerMock.Object,
+                this.patientOrgReferenceStorageBrokerMock.Object,
+                this.loggingBrokerMock.Object)
+            { CallBase = true };
+
+            AccessRequest someAccessRequest = CreateRandomAccessRequest();
+            var serviceException = new Exception();
+
+            var failedServiceAccessOrchestrationException =
+                new FailedServiceAccessOrchestrationException(
+                    message: "Failed service access orchestration error occurred, contact support.",
+                    innerException: serviceException);
+
+            var expectedAccessOrchestrationServiceException =
+                new AccessOrchestrationServiceException(
+                    message: "Service error occurred, contact support.",
+                    innerException: failedServiceAccessOrchestrationException);
+
+            accessOrchestrationServiceMock.Setup(service =>
+                 service.GetOrganisationsForUserAsync(It.IsAny<string>()))
+                     .ThrowsAsync(serviceException);
+
+            AccessOrchestrationService accessOrchestrationService = accessOrchestrationServiceMock.Object;
+
+            // when
+            ValueTask<AccessRequest> validateAccessForIdentificationRequestTask =
+                accessOrchestrationService
+                    .ValidateAccessForIdentificationRequestsAsync(someAccessRequest);
+
+            AccessOrchestrationServiceException
+                actualAccessOrchestrationServiceException =
+                await Assert.ThrowsAsync<AccessOrchestrationServiceException>(
+                    validateAccessForIdentificationRequestTask.AsTask);
+
+            // then
+            actualAccessOrchestrationServiceException
+                .Should().BeEquivalentTo(expectedAccessOrchestrationServiceException);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogErrorAsync(It.Is(SameExceptionAs(
+                   expectedAccessOrchestrationServiceException))),
                        Times.Once);
 
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
