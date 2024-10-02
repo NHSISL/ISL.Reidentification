d98f1cdcfac2c844f73a60f55e99a31f69037262
Joe Parkes <joe.owens@answerdigital.com>
2024-10-02 13:15:51 +0100

    Adding true and false tests for ValidateAccessForIdentificationRequests

--- ISL.ReIdentification.Core.Tests.Unit/Services/Orchestrations/Accesses/AccessOrchestrationServiceTests.Logic.ValidateAccessForIdentificationRequests.cs
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Orchestrations/Accesses/AccessOrchestrationServiceTests.Logic.ValidateAccessForIdentificationRequests.cs
@@ -14,7 +14,7 @@ namespace ISL.ReIdentification.Core.Tests.Unit.Services.Orchestrations.Accesses
     public partial class AccessOrchestrationServiceTests
     {
         [Fact]
-        public async Task ShouldValidateAccessForIdentificationRequests()
+        public async Task ShouldSetHasAccessToTrueIfUserHasAccessValidateAccessForIdentificationRequests()
         {
             // given
             var accessOrchestrationServiceMock = new Mock<AccessOrchestrationService>
@@ -37,6 +37,60 @@ namespace ISL.ReIdentification.Core.Tests.Unit.Services.Orchestrations.Accesses
                 new List<string> { userOrganisation };
 
             bool userHasAccessToPatientResult = true;
+
+            accessOrchestrationServiceMock.Setup(service =>
+                service.GetOrganisationsForUserAsync(inputUserEmail))
+                    .ReturnsAsync(userOrganisations);
+
+            accessOrchestrationServiceMock.Setup(service =>
+                service.UserHasAccessToPatientAsync(inputIdentifier, userOrganisations))
+                    .ReturnsAsync(userHasAccessToPatientResult);
+
+            AccessOrchestrationService accessOrchestrationService = accessOrchestrationServiceMock.Object;
+            AccessRequest expectedAccessRequest = accessRequest;
+
+            // when
+            AccessRequest actualAccessRequest =
+                await accessOrchestrationService.ValidateAccessForIdentificationRequestsAsync(accessRequest);
+
+            // then
+            actualAccessRequest.Should().Be(expectedAccessRequest);
+
+            accessOrchestrationServiceMock.Verify(service =>
+                service.GetOrganisationsForUserAsync(inputUserEmail),
+                    Times.Once);
+
+            accessOrchestrationServiceMock.Verify(service =>
+                service.UserHasAccessToPatientAsync(inputIdentifier, userOrganisations),
+                    Times.Once);
+
+            accessOrchestrationServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldSetHasAccessToFalseIfUserDoesNotHaveAccessValidateAccessForIdentificationRequests()
+        {
+            // given
+            var accessOrchestrationServiceMock = new Mock<AccessOrchestrationService>
+                (this.dateTimeBrokerMock.Object,
+                this.reIdentificationStorageBrokerMock.Object,
+                this.patientOrgReferenceStorageBrokerMock.Object,
+                this.loggingBrokerMock.Object)
+            { CallBase = true };
+
+            string userEmail = GetRandomStringWithLength(10);
+            string inputUserEmail = userEmail;
+            string identifier = GetRandomStringWithLength(5);
+            string inputIdentifier = identifier;
+            AccessRequest accessRequest = CreateRandomAccessRequest();
+            accessRequest.IdentificationRequest.UserIdentifier = userEmail;
+            accessRequest.IdentificationRequest.IdentificationItems[0].Identifier = inputIdentifier;
+            string userOrganisation = GetRandomStringWithLength(5);
+
+            List<string> userOrganisations =
+                new List<string> { userOrganisation };
+
+            bool userHasAccessToPatientResult = false;
 
             accessOrchestrationServiceMock.Setup(service =>
                 service.GetOrganisationsForUserAsync(inputUserEmail))
