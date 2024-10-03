df1b71df6b3f5d8da9db1b06c0932e8aa6ad3308
Christo du Toit <christo@dutoit.co.uk>
2024-10-03 16:05:02 +0100

    BROKERS: NotificationBroker - SendEmailAsync

---
+++ ISL.ReIdentification.Core/Brokers/Notifications/INotificationBroker.cs
@@ -0,0 +1,26 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ISL.ReIdentification.Core.Brokers.Notifications
+{
+    internal interface INotificationBroker
+    {
+        /// <summary>
+        /// Sends an email to the specified email address with the specified
+        /// subject, body and personalisation items.
+        /// </summary>
+        /// <returns>A string representing the unique identifier of the sent email.</returns>
+        /// <exception cref="NotificationValidationProviderException" />
+        /// <exception cref="NotificationDependencyProviderException" />
+        /// <exception cref="NotificationServiceProviderException" />
+        public ValueTask<string> SendEmailAsync(
+            string toEmail,
+            string subject,
+            string body,
+            Dictionary<string, dynamic> personalisation);
+    }
+}
---
+++ ISL.ReIdentification.Core/Brokers/Notifications/NotificationBroker.cs
@@ -0,0 +1,35 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ISL.Providers.Notifications.Abstractions;
+
+namespace ISL.ReIdentification.Core.Brokers.Notifications
+{
+    public class NotificationBroker : INotificationBroker
+    {
+        private readonly INotificationAbstractionProvider notificationAbstractionProvider;
+
+        public NotificationBroker(INotificationAbstractionProvider notificationAbstractionProvider)
+        {
+            this.notificationAbstractionProvider = notificationAbstractionProvider;
+        }
+
+        /// <summary>
+        /// Sends an email to the specified email address with the specified
+        /// subject, body and personalisation items.
+        /// </summary>
+        /// <returns>A string representing the unique identifier of the sent email.</returns>
+        /// <exception cref="NotificationValidationProviderException" />
+        /// <exception cref="NotificationDependencyProviderException" />
+        /// <exception cref="NotificationServiceProviderException" />
+        public async ValueTask<string> SendEmailAsync(
+            string toEmail,
+            string subject,
+            string body,
+            Dictionary<string, dynamic> personalisation) =>
+            await this.notificationAbstractionProvider.SendEmailAsync(toEmail, subject, body, personalisation);
+    }
+}
