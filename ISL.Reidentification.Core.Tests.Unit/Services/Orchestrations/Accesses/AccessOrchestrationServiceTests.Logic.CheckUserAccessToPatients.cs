b9257e70e46a1e97518fba3c6aae5b33b2fe056a
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-10-04 15:09:29 +0100

    ShouldSetHasAccessOnCheckUserAccessToPatientsAsync -> PASS

--- ISL.Reidentification.Core.Tests.Unit/Services/Orchestrations/Accesses/AccessOrchestrationServiceTests.Logic.CheckUserAccessToPatients.cs
+++ ISL.Reidentification.Core.Tests.Unit/Services/Orchestrations/Accesses/AccessOrchestrationServiceTests.Logic.CheckUserAccessToPatients.cs
@@ -14,8 +14,10 @@ namespace ISL.ReIdentification.Core.Tests.Unit.Services.Orchestrations.Accesses
 {
     public partial class AccessOrchestrationServiceTests
     {
-        [Fact]
-        public async Task ShouldSetHasAccessToTrueIfUserHasAccessValidateAccessForIdentificationRequests()
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task ShouldSetHasAccessOnCheckUserAccessToPatientsAsync(bool userHasAccess)
         {
             // given
             var accessOrchestrationServiceMock = new Mock<AccessOrchestrationService>
@@ -28,7 +30,7 @@ namespace ISL.ReIdentification.Core.Tests.Unit.Services.Orchestrations.Accesses
             AccessRequest randomRequest = CreateRandomAccessRequest();
             AccessRequest inputAccessRequest = randomRequest.DeepClone();
             AccessRequest outputAccessRequest = inputAccessRequest.DeepClone();
-            outputAccessRequest.IdentificationRequest.IdentificationItems.ForEach(item => item.HasAccess = true);
+            outputAccessRequest.IdentificationRequest.IdentificationItems.ForEach(item => item.HasAccess = userHasAccess);
             AccessRequest expectedAccessRequest = outputAccessRequest.DeepClone();
 
             string userOrganisation = GetRandomStringWithLength(5);
@@ -36,58 +38,9 @@ namespace ISL.ReIdentification.Core.Tests.Unit.Services.Orchestrations.Accesses
             List<string> userOrganisations =
                 new List<string> { userOrganisation };
 
-            bool userHasAccessToPatientResult = true;
-
             accessOrchestrationServiceMock.Setup(service =>
                 service.UserHasAccessToPatientAsync(It.IsAny<string>(), It.IsAny<List<string>>()))
-                    .ReturnsAsync(userHasAccessToPatientResult);
-
-            AccessOrchestrationService service = accessOrchestrationServiceMock.Object;
-
-            // when
-            AccessRequest actualAccessRequest =
-                await service.CheckUserAccessToPatientsAsync(inputAccessRequest, userOrganisations);
-
-            // then
-            actualAccessRequest.Should().BeEquivalentTo(expectedAccessRequest);
-
-            accessOrchestrationServiceMock.Verify(service =>
-                service.CheckUserAccessToPatientsAsync(inputAccessRequest, userOrganisations),
-                    Times.Once());
-
-            accessOrchestrationServiceMock.Verify(service =>
-                service.UserHasAccessToPatientAsync(It.IsAny<string>(), It.IsAny<List<string>>()),
-                    Times.Exactly(inputAccessRequest.IdentificationRequest.IdentificationItems.Count));
-
-            accessOrchestrationServiceMock.VerifyNoOtherCalls();
-        }
-
-        [Fact]
-        public async Task ShouldSetHasAccessToFalseIfUserDoesNotHaveAccessValidateAccessForIdentificationRequests()
-        {
-            // given
-            var accessOrchestrationServiceMock = new Mock<AccessOrchestrationService>
-                (this.userAccessServiceMock.Object,
-                this.pdsDataServiceMock.Object,
-                this.dateTimeBrokerMock.Object,
-                this.loggingBrokerMock.Object)
-            { CallBase = true };
-
-            AccessRequest randomRequest = CreateRandomAccessRequest();
-            AccessRequest inputAccessRequest = randomRequest.DeepClone();
-            AccessRequest outputAccessRequest = inputAccessRequest.DeepClone();
-            AccessRequest expectedAccessRequest = outputAccessRequest.DeepClone();
-
-            string userOrganisation = GetRandomStringWithLength(5);
-
-            List<string> userOrganisations =
-                new List<string> { userOrganisation };
-
-            bool userHasAccessToPatientResult = false;
-
-            accessOrchestrationServiceMock.Setup(service =>
-                service.UserHasAccessToPatientAsync(It.IsAny<string>(), It.IsAny<List<string>>()))
-                    .ReturnsAsync(userHasAccessToPatientResult);
+                    .ReturnsAsync(userHasAccess);
 
             AccessOrchestrationService service = accessOrchestrationServiceMock.Object;
 
