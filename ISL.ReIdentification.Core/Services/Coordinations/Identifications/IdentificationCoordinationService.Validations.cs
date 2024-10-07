5b85dac81c1e158161070c1abebd5322fa846e7c
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-10-07 15:08:12 +0100

    Revert "Revert "Merge remote-tracking branch 'origin/main' into users/JParkesAD/orchestrations-access-userhasaccess""
    This reverts commit f4a3fe9831051ad6b85ac14d78d3f6a37d035984.

---
+++ ISL.ReIdentification.Core/Services/Coordinations/Identifications/IdentificationCoordinationService.Validations.cs
@@ -0,0 +1,36 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using ISL.ReIdentification.Core.Models.Foundations.ReIdentifications;
+using ISL.ReIdentification.Core.Models.Foundations.ReIdentifications.Exceptions;
+using ISL.ReIdentification.Core.Models.Orchestrations.Accesses;
+using ISL.ReIdentification.Core.Models.Orchestrations.Accesses.Exceptions;
+
+namespace ISL.ReIdentification.Core.Services.Orchestrations.Identifications
+{
+    public partial class IdentificationCoordinationService : IIdentificationCoordinationService
+    {
+        private static void ValidateOnProcessIdentificationRequests(AccessRequest accessRequest)
+        {
+            ValidateAccessRequestIsNotNull(accessRequest);
+            ValidateIdentificationRequestIsNotNull(accessRequest.IdentificationRequest);
+        }
+
+        private static void ValidateAccessRequestIsNotNull(AccessRequest accessRequest)
+        {
+            if (accessRequest is null)
+            {
+                throw new NullAccessRequestException("Access request is null.");
+            }
+        }
+
+        private static void ValidateIdentificationRequestIsNotNull(IdentificationRequest identificationRequest)
+        {
+            if (identificationRequest is null)
+            {
+                throw new NullIdentificationRequestException("Identification request is null.");
+            }
+        }
+    }
+}
---
+++ ISL.Reidentification.Core.Tests.Unit/Services/Coordinations/Identifications/IdentificationCoordinationTests.cs
@@ -0,0 +1,140 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Linq.Expressions;
+using ISL.ReIdentification.Core.Brokers.Loggings;
+using ISL.ReIdentification.Core.Models.Foundations.DelegatedAccesses;
+using ISL.ReIdentification.Core.Models.Foundations.ReIdentifications;
+using ISL.ReIdentification.Core.Models.Orchestrations.Accesses;
+using ISL.ReIdentification.Core.Models.Orchestrations.Accesses.Exceptions;
+using ISL.ReIdentification.Core.Models.Orchestrations.Identifications.Exceptions;
+using ISL.ReIdentification.Core.Services.Orchestrations.Accesses;
+using ISL.ReIdentification.Core.Services.Orchestrations.Identifications;
+using KellermanSoftware.CompareNetObjects;
+using Moq;
+using Tynamix.ObjectFiller;
+using Xeptions;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Coordinations.Identifications
+{
+    public partial class IdentificationCoordinationTests
+    {
+        private readonly Mock<IIdentificationOrchestrationService> identificationOrchestrationServiceMock;
+        private readonly Mock<IAccessOrchestrationService> accessOrchestrationServiceMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IdentificationCoordinationService identificationCoordinationService;
+        private readonly ICompareLogic compareLogic;
+
+        public IdentificationCoordinationTests()
+        {
+            this.identificationOrchestrationServiceMock = new Mock<IIdentificationOrchestrationService>();
+            this.accessOrchestrationServiceMock = new Mock<IAccessOrchestrationService>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            this.compareLogic = new CompareLogic();
+
+            this.identificationCoordinationService = new IdentificationCoordinationService(
+                this.accessOrchestrationServiceMock.Object,
+                this.identificationOrchestrationServiceMock.Object,
+                this.loggingBrokerMock.Object);
+        }
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+
+        private static AccessRequest CreateRandomAccessRequest() =>
+            CreateAccessRequestFiller().Create();
+
+        private static Filler<AccessRequest> CreateAccessRequestFiller()
+        {
+            var filler = new Filler<AccessRequest>();
+
+            filler.Setup()
+                .OnProperty(request => request.DelegatedAccessRequest).Use(CreateRandomDelegatedAccess);
+
+            return filler;
+        }
+
+        private static IdentificationRequest CreateRandomIdentificationRequest() =>
+            CreateIdentificationRequestFiller().Create();
+
+        private static Filler<IdentificationRequest> CreateIdentificationRequestFiller() =>
+            new Filler<IdentificationRequest>();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static DelegatedAccess CreateRandomDelegatedAccess() =>
+            CreateRandomDelegatedAccess(dateTimeOffset: GetRandomDateTimeOffset());
+
+        private static DelegatedAccess CreateRandomDelegatedAccess(DateTimeOffset dateTimeOffset) =>
+            CreateDelegatedAccessesFiller(dateTimeOffset).Create();
+
+        private static Filler<DelegatedAccess> CreateDelegatedAccessesFiller(DateTimeOffset dateTimeOffset)
+        {
+            var filler = new Filler<DelegatedAccess>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(dateTimeOffset);
+
+            return filler;
+        }
+
+        private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+            actualException => actualException.SameExceptionAs(expectedException);
+
+        public static TheoryData<Xeption> DependencyValidationExceptions()
+        {
+            string randomMessage = GetRandomString();
+            string exceptionMessage = randomMessage;
+            var innerException = new Xeption(exceptionMessage);
+
+            return new TheoryData<Xeption>
+            {
+                new AccessOrchestrationValidationException(
+                    message: "Access orchestration validation errors occured, please try again.",
+                    innerException),
+
+                new AccessOrchestrationDependencyValidationException(
+                    message: "Access orchestration dependency validation occurred, please try again.",
+                    innerException),
+
+                new IdentificationOrchestrationValidationException(
+                    message: "Identification orchestration validation errors occurred, please try again.",
+                    innerException),
+
+                new IdentificationOrchestrationDependencyValidationException(
+                    message: "Identification orchestration dependency validation occurred, please try again.",
+                    innerException),
+            };
+        }
+
+        public static TheoryData<Xeption> DependencyExceptions()
+        {
+            string randomMessage = GetRandomString();
+            string exceptionMessage = randomMessage;
+            var innerException = new Xeption(exceptionMessage);
+
+            return new TheoryData<Xeption>
+            {
+                new AccessOrchestrationServiceException(
+                    message: "Access orchestration service error occurred, please contact support.",
+                    innerException),
+
+                new AccessOrchestrationDependencyException(
+                    message: "Access orchestration dependency error occurred, please contact support.",
+                    innerException),
+
+                new IdentificationOrchestrationServiceException(
+                    message: "Identification orchestration service error occurred, please contact support.",
+                    innerException),
+
+                new IdentificationOrchestrationDependencyException(
+                    message: "Identification orchestration dependency error occurred, please contact support.",
+                    innerException),
+            };
+        }
+    }
+}
