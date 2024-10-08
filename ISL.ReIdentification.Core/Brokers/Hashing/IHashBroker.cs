2b3b626617cb2cfdd3f67108c4a60181f7f8d069
Joe Parkes <joe.owens@answerdigital.com>
2024-10-08 14:38:13 +0100

    BROKERS: HashBroker

---
+++ ISL.ReIdentification.Core/Brokers/Hashing/IHashBroker.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.IO;
+
+namespace ISL.ReIdentification.Core.Brokers.Hashing
+{
+    public interface IHashBroker
+    {
+        string GenerateMd5Hash(Stream? data);
+        string GenerateSha256Hash(Stream? data);
+    }
+}
