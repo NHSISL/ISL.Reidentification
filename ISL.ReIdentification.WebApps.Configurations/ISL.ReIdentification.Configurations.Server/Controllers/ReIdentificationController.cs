227bb678b3a5754f986327e4d59acf6262e64418
Joe Parkes <joe.owens@answerdigital.com>
2024-10-09 15:31:59 +0100

    Create ReIdentificationController with PostIdentificationRequests endpoint

---
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server/Controllers/ReIdentificationController.cs
@@ -0,0 +1,23 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ISL.ReIdentification.Core.Models.Foundations.ReIdentifications;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace ISL.ReIdentification.Configurations.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReIdentificationController : RESTFulController
+    {
+        [HttpPost]
+        public async ValueTask<ActionResult<List<string>>>
+            PostIdentificationRequestsAsync(List<IdentificationRequest> identificationRequests) =>
+                throw new NotImplementedException();
+    }
+}
