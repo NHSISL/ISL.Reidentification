fa2dfcf9324035846063726ccc632fd9532947cc
Christo du Toit <christo@dutoit.co.uk>
2024-10-09 15:28:15 +0100

    DATA: Re-applied migrations

--- ISL.Reidentification.Core/Models/Foundations/AccessAudit/Exceptions/FailedServiceAccessAuditException.cs
+++ ISL.Reidentification.Core/Models/Foundations/AccessAudit/Exceptions/FailedServiceAccessAuditException.cs
@@ -5,19 +5,11 @@
 using System;
 using Xeptions;
 
-<<<<<<< HEAD:ISL.ReIdentification.Core/Models/Foundations/AccessAudit/Exceptions/FailedServiceAccessAuditException.cs
 namespace ISL.ReIdentification.Core.Models.Foundations.AccessAudits.Exceptions
 {
     public class FailedServiceAccessAuditException : Xeption
     {
         public FailedServiceAccessAuditException(string message, Exception innerException)
-=======
-namespace ISL.ReIdentification.Core.Models.Foundations.ImpersonationContexts.Exceptions
-{
-    public class FailedServiceImpersonationContextException : Xeption
-    {
-        public FailedServiceImpersonationContextException(string message, Exception innerException)
->>>>>>> MAJOR CODE RUB:  Rename DelegatedAccess to ImpersonationContext, moved ODS and PDS to ReIdentificationStorageBroker, Unified migrations:ISL.ReIdentification.Core/Models/Foundations/ImpersonationContexts/Exceptions/FailedServiceImpersonationContextException.cs
             : base(message, innerException)
         { }
     }
--- ISL.Reidentification.Core/Models/Foundations/AccessAudit/Exceptions/InvalidAccessAuditException.cs
+++ ISL.Reidentification.Core/Models/Foundations/AccessAudit/Exceptions/InvalidAccessAuditException.cs
@@ -4,19 +4,11 @@
 
 using Xeptions;
 
-<<<<<<< HEAD:ISL.ReIdentification.Core/Models/Foundations/AccessAudit/Exceptions/InvalidAccessAuditException.cs
 namespace ISL.ReIdentification.Core.Models.Foundations.AccessAudits.Exceptions
 {
     public class InvalidAccessAuditException : Xeption
     {
         public InvalidAccessAuditException(string message)
-=======
-namespace ISL.ReIdentification.Core.Models.Foundations.ImpersonationContexts.Exceptions
-{
-    public class NotFoundImpersonationContextException : Xeption
-    {
-        public NotFoundImpersonationContextException(string message)
->>>>>>> MAJOR CODE RUB:  Rename DelegatedAccess to ImpersonationContext, moved ODS and PDS to ReIdentificationStorageBroker, Unified migrations:ISL.ReIdentification.Core/Models/Foundations/ImpersonationContexts/Exceptions/NotFoundImpersonationContextException.cs
             : base(message)
         { }
     }
